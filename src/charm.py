612610ef275a94c7def5e6b0ab9a1d0b9de84abe
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2022-07-17 22:14:17 -0300

    Revert "Add replication and scaling"
    This reverts commit a2f7e332230aa680bdfa7b6061112c84014bf410.

--- src/charm.py
+++ src/charm.py
@@ -11,13 +11,7 @@ import subprocess
 from typing import List
 
 from charms.operator_libs_linux.v0 import apt
-from ops.charm import (
-    ActionEvent,
-    CharmBase,
-    LeaderElectedEvent,
-    RelationChangedEvent,
-    RelationDepartedEvent,
-)
+from ops.charm import ActionEvent, CharmBase
 from ops.main import main
 from ops.model import (
     ActiveStatus,
