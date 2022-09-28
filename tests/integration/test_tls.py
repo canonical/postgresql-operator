df5b66c1bff73f080c320cea4a23ae6f06e2c1d8
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2022-09-28 18:09:38 -0300

    Add test for TLS being used on pg_rewind connections

--- tests/integration/test_tls.py
+++ tests/integration/test_tls.py
@@ -3,12 +3,20 @@
 # See LICENSE file for licensing details.
 import pytest as pytest
 from pytest_operator.plugin import OpsTest
+from tenacity import Retrying, stop_after_delay, wait_exponential
 
 from tests.helpers import METADATA
 from tests.integration.helpers import (
     DATABASE_APP_NAME,
     check_tls,
     check_tls_patroni_api,
+    db_connect,
+    enable_connections_logging,
+    get_password,
+    get_primary,
+    get_unit_address,
+    primary_changed,
+    run_command_on_unit,
 )
 
 APP_NAME = METADATA["name"]
@@ -48,6 +56,60 @@ async def test_tls_enabled(ops_test: OpsTest) -> None:
         for unit in ops_test.model.applications[DATABASE_APP_NAME].units:
             assert await check_tls(ops_test, unit.name, enabled=True)
             assert await check_tls_patroni_api(ops_test, unit.name, enabled=True)
+
+        # Test TLS being used by pg_rewind. To accomplish that, get the primary unit
+        # and a replica that will be promoted to primary (this should trigger a rewind
+        # operation when the old primary is started again).
+        any_unit = ops_test.model.applications[DATABASE_APP_NAME].units[0].name
+        primary = await get_primary(ops_test, any_unit)
+        replica = [
+            unit.name
+            for unit in ops_test.model.applications[DATABASE_APP_NAME].units
+            if unit.name != primary
+        ][0]
+
+        # Enable additional logs on the PostgreSQL instance to check TLS
+        # being used in a later step.
+        enable_connections_logging(ops_test, primary)
+
+        # Promote the replica to primary.
+        await run_command_on_unit(
+            ops_test,
+            replica,
+            "su -c '/usr/lib/postgresql/12/bin/pg_ctl -D /var/lib/postgresql/data/pgdata promote' postgres",
+        )
+
+        # Write some data to the initial primary (this causes a divergence
+        # in the instances' timelines).
+        host = get_unit_address(ops_test, primary)
+        password = await get_password(ops_test, primary)
+        with db_connect(host, password) as connection:
+            connection.autocommit = True
+            with connection.cursor() as cursor:
+                cursor.execute("CREATE TABLE pgrewindtest (testcol INT);")
+        connection.close()
+
+        # Stop the initial primary.
+        await run_command_on_unit(ops_test, primary, "systemctl stop patroni")
+
+        # Check that the primary changed.
+        assert await primary_changed(ops_test, primary), "primary not changed"
+
+        # Restart the initial primary and check the logs to ensure TLS is being used by pg_rewind.
+        # It can take some time for the rewind operation to happen.
+        for attempt in Retrying(
+            stop=stop_after_delay(60 * 5), wait=wait_exponential(multiplier=1, min=2, max=30)
+        ):
+            with attempt:
+                await run_command_on_unit(ops_test, primary, "systemctl start patroni")
+                logs = await run_command_on_unit(
+                    ops_test, replica, "journalctl -u patroni.service"
+                )
+                assert (
+                    "connection authorized: user=rewind database=postgres SSL enabled"
+                    " (protocol=TLSv1.3, cipher=TLS_AES_256_GCM_SHA384, bits=256, compression=off)"
+                    in logs
+                ), "TLS is not being used on pg_rewind connections"
 
         # Remove the relation.
         await ops_test.model.applications[DATABASE_APP_NAME].remove_relation(
