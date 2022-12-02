91e4abd9903577de57f86708e4c596a8ad03b747
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2022-12-02 00:00:21 -0300

    Replace ps aux | grep with pgrep

--- tests/integration/ha_tests/helpers.py
+++ tests/integration/ha_tests/helpers.py
@@ -46,17 +46,10 @@ async def all_db_processes_down(ops_test: OpsTest, process: str) -> bool:
         for attempt in Retrying(stop=stop_after_delay(60), wait=wait_fixed(3)):
             with attempt:
                 for unit in ops_test.model.applications[app].units:
-                    search_db_process = f'run --unit {unit.name} ps ax | grep "{process} "'
-                    _, processes, _ = await ops_test.juju(*search_db_process.split())
+                    _, raw_pid, _ = await ops_test.juju("ssh", unit.name, "pgrep", "-f", process)
 
-                    # `ps ax | grep "{DB_PROCESS} "` is a process on its own and will be shown in
-                    # the output of ps aux, hence it is important that we check if there is
-                    # more than one process containing the name `DB_PROCESS`
-                    # splitting processes by "\n" results in one or more empty lines, hence we
-                    # need to process these lines accordingly.
-                    processes = [proc for proc in processes.split("\n") if len(proc) > 0]
-
-                    if len(processes) > 1:
+                    # If something was returned, there is a running process.
+                    if len(raw_pid) > 0:
                         raise ProcessRunningError
     except RetryError:
         return False
