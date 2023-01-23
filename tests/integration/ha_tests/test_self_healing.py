cc7f1996c0c131c9c9080fb5e6ba8507c27326b9
Dragomir Penev <dragomir.penev@canonical.com>
2023-01-23 18:10:26 +0200

    Pass prebuilt charm to tests for local execution

--- tests/integration/ha_tests/test_self_healing.py
+++ tests/integration/ha_tests/test_self_healing.py
@@ -42,14 +42,18 @@ DB_PROCESSES = [POSTGRESQL_PROCESS, PATRONI_PROCESS]
 
 @pytest.mark.abort_on_fail
 @pytest.mark.ha_self_healing_tests
-async def test_build_and_deploy(ops_test: OpsTest) -> None:
+async def test_build_and_deploy(ops_test: OpsTest, cmd_postgresql_charm_charm) -> None:
     """Build and deploy three unit of PostgreSQL."""
     # It is possible for users to provide their own cluster for HA testing. Hence, check if there
     # is a pre-existing cluster.
     if await app_name(ops_test):
         return
 
-    charm = await ops_test.build_charm(".")
+    if cmd_postgresql_charm_charm:
+        charm = cmd_postgresql_charm_charm
+    else:
+        charm = await ops_test.build_charm(".")
+
     async with ops_test.fast_forward():
         await ops_test.model.deploy(charm, resources={"patroni": "patroni.tar.gz"}, num_units=3)
         await ops_test.juju("attach-resource", APP_NAME, "patroni=patroni.tar.gz")
