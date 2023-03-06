4e8178b7650b9b5a879cc2fd9fee482accd28b9d
Dragomir Penev <dragomir.penev@canonical.com>
2023-03-06 19:02:16 +0200

    Code review tweaks

--- tests/integration/ha_tests/application-charm/src/continuous_writes.py
+++ tests/integration/ha_tests/application-charm/src/continuous_writes.py
@@ -44,7 +44,6 @@ def continuous_writes(starting_number: int):
         try:
             with psycopg2.connect(connection_string) as connection, connection.cursor() as cursor:
                 connection.autocommit = True
-                cursor.execute("SET synchronous_commit = 'remote_apply';")
                 cursor.execute(f"INSERT INTO continuous_writes(number) VALUES({write_value});")
         except (
             psycopg2.InterfaceError,
