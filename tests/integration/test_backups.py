3d0ce0b6841bc7b6f6f0ef4ff03f421f78a28ed7
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2023-03-31 13:26:06 -0300

    Fix restore step on test

--- tests/integration/test_backups.py
+++ tests/integration/test_backups.py
@@ -101,50 +101,54 @@ async def test_backup(ops_test: OpsTest, cloud_configs: Tuple[Dict, Dict]) -> No
             connection.cursor().execute("CREATE TABLE backup_table_2 (test_collumn INT );")
         connection.close()
 
-        # # Scale down to be able to restore.
-        # await ops_test.model.units.get(replica).remove()
-        #
-        # # Run the "restore backup" action.
-        # for attempt in Retrying(
-        #     stop=stop_after_attempt(10), wait=wait_exponential(multiplier=1, min=2, max=30)
-        # ):
-        #     with attempt:
-        #         logger.info("restoring the backup")
-        #         most_recent_backup = backups.split("\n")[-1]
-        #         backup_id = most_recent_backup.split()[0]
-        #         action = await ops_test.model.units.get(f"{database_app_name}/0").run_action(
-        #             "restore", **{"backup-id": backup_id}
-        #         )
-        #         await action.wait()
-        #         restore_status = action.results.get("restore-status")
-        #         assert restore_status, "restore hasn't succeeded"
-        #
-        # # Wait for the restore to complete.
-        # async with ops_test.fast_forward():
-        #     await ops_test.model.wait_for_idle(status="active", timeout=1000)
-        #
-        # # Check that the backup was correctly restored by having only the first created table.
-        # primary = await get_primary(ops_test, f"{database_app_name}/0")
-        # address = get_unit_address(ops_test, primary)
-        # logger.info("checking that the backup was correctly restored")
-        # with db_connect(
-        #     host=address, password=password
-        # ) as connection, connection.cursor() as cursor:
-        #     cursor.execute(
-        #         "SELECT EXISTS (SELECT FROM information_schema.tables"
-        #         " WHERE table_schema = 'public' AND table_name = 'backup_table_1');"
-        #     )
-        #     assert cursor.fetchone()[
-        #         0
-        #     ], "backup wasn't correctly restored: table 'backup_table_1' doesn't exist"
-        #     cursor.execute(
-        #         "SELECT EXISTS (SELECT FROM information_schema.tables"
-        #         " WHERE table_schema = 'public' AND table_name = 'backup_table_2');"
-        #     )
-        #     assert not cursor.fetchone()[
-        #         0
-        #     ], "backup wasn't correctly restored: table 'backup_table_2' exists"
-        # connection.close()
-        #
-        # # Remove the database app.
-        # await ops_test.model.applications[database_app_name].remove()
+        # Scale down to be able to restore.
+        async with ops_test.fast_forward():
+            await ops_test.model.units.get(replica).remove()
+            await ops_test.model.block_until(
+                lambda: len(ops_test.model.applications[database_app_name].units) == 1
+            )
+
+        # Run the "restore backup" action.
+        for attempt in Retrying(
+            stop=stop_after_attempt(10), wait=wait_exponential(multiplier=1, min=2, max=30)
+        ):
+            with attempt:
+                logger.info("restoring the backup")
+                most_recent_backup = backups.split("\n")[-1]
+                backup_id = most_recent_backup.split()[0]
+                action = await ops_test.model.units.get(f"{database_app_name}/0").run_action(
+                    "restore", **{"backup-id": backup_id}
+                )
+                await action.wait()
+                restore_status = action.results.get("restore-status")
+                assert restore_status, "restore hasn't succeeded"
+
+        # Wait for the restore to complete.
+        async with ops_test.fast_forward():
+            await ops_test.model.wait_for_idle(status="active", timeout=1000)
+
+        # Check that the backup was correctly restored by having only the first created table.
+        primary = await get_primary(ops_test, f"{database_app_name}/0")
+        address = get_unit_address(ops_test, primary)
+        logger.info("checking that the backup was correctly restored")
+        with db_connect(
+            host=address, password=password
+        ) as connection, connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT EXISTS (SELECT FROM information_schema.tables"
+                " WHERE table_schema = 'public' AND table_name = 'backup_table_1');"
+            )
+            assert cursor.fetchone()[
+                0
+            ], "backup wasn't correctly restored: table 'backup_table_1' doesn't exist"
+            cursor.execute(
+                "SELECT EXISTS (SELECT FROM information_schema.tables"
+                " WHERE table_schema = 'public' AND table_name = 'backup_table_2');"
+            )
+            assert not cursor.fetchone()[
+                0
+            ], "backup wasn't correctly restored: table 'backup_table_2' exists"
+        connection.close()
+
+        # Remove the database app.
+        await ops_test.model.applications[database_app_name].remove()
