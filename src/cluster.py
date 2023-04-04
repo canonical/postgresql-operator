23757c241a94b40c9672efb54728bd7f2166577d
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2023-04-04 16:37:02 -0300

    Add lag key fallback

--- src/cluster.py
+++ src/cluster.py
@@ -367,7 +367,7 @@ class Patroni:
 
         for member in cluster_status.json()["members"]:
             if member["name"] == self.member_name:
-                return member["lag"]
+                return member.get("lag", "")
 
         return "unknown"
 
