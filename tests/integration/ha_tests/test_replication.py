909a400c2555c8906db567e6163da010a2a83a5d
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2023-04-07 13:49:29 -0300

    Add consistency test

--- tests/integration/ha_tests/test_replication.py
+++ tests/integration/ha_tests/test_replication.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
+from time import sleep
 
 import pytest
 from pytest_operator.plugin import OpsTest
@@ -76,8 +77,8 @@ async def test_reelection(ops_test: OpsTest, continuous_writes) -> None:
     for unit in ops_test.model.applications[app].units:
         if unit.name != new_primary_name:
             assert await secondary_up_to_date(
-                ops_test, unit_name, total_expected_writes
-            ), "secondary not up to date with the cluster after restarting."
+                ops_test, unit.name, total_expected_writes
+            ), f"secondary {unit.name} not up to date with the cluster after reelection."
 
 
 async def test_consistency(ops_test: OpsTest, continuous_writes) -> None:
@@ -85,6 +86,21 @@ async def test_consistency(ops_test: OpsTest, continuous_writes) -> None:
     app = await app_name(ops_test)
     if len(ops_test.model.applications[app].units) < 3:
         await scale_application(ops_test, app, 3)
+
+    # Start an application that continuously writes data to the database.
+    await start_continuous_writes(ops_test, app)
+
+    # Wait some time.
+    sleep(5)
+
+    # Verify that no writes to the database were missed.
+    total_expected_writes = await check_writes(ops_test)
+
+    # Verify that all the units are up-to-date.
+    for unit in ops_test.model.applications[app].units:
+        assert await secondary_up_to_date(
+            ops_test, unit.name, total_expected_writes
+        ), f"unit {unit.name} not up to date."
 
 
 async def test_no_data_replicated_between_clusters(ops_test: OpsTest, continuous_writes) -> None:
