5fcb0c3a4ca53a726ad2bb6087500580ffcb1746
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2023-06-07 10:22:48 -0300

    Add unit test for set_up_relation method

--- tests/unit/test_db.py
+++ tests/unit/test_db.py
@@ -81,8 +81,7 @@ class TestDbProvides(unittest.TestCase):
             {"database": DATABASE},
         )
 
-    @patch("charm.DbProvides.update_endpoints")
-    @patch("relations.db.new_password", return_value="test-password")
+    @patch("charm.DbProvides.set_up_relation")
     @patch.object(EventBase, "defer")
     @patch(
         "charm.PostgresqlOperatorCharm.primary_endpoint",
@@ -94,84 +93,41 @@ class TestDbProvides(unittest.TestCase):
         _member_started,
         _primary_endpoint,
         _defer,
-        _new_password,
-        _update_endpoints,
+        _set_up_relation,
     ):
-        with patch.object(PostgresqlOperatorCharm, "postgresql", Mock()) as postgresql_mock:
-            # Set some side effects to test multiple situations.
-            _member_started.side_effect = [False, True, True, True, True, True]
-            _primary_endpoint.side_effect = [
-                None,
-                {"1.1.1.1"},
-                {"1.1.1.1"},
-                {"1.1.1.1"},
-                {"1.1.1.1"},
-            ]
-            postgresql_mock.create_user = PropertyMock(
-                side_effect=[None, PostgreSQLCreateUserError, None, None]
-            )
-            postgresql_mock.create_database = PropertyMock(
-                side_effect=[None, PostgreSQLCreateDatabaseError, None]
-            )
-            postgresql_mock.get_postgresql_version = PropertyMock(
-                side_effect=[
-                    POSTGRESQL_VERSION,
-                    PostgreSQLGetPostgreSQLVersionError,
-                ]
-            )
+        # Set some side effects to test multiple situations.
+        _member_started.side_effect = [False, True, True, True, True, True]
+        _primary_endpoint.side_effect = [
+            None,
+            {"1.1.1.1"},
+            {"1.1.1.1"},
+            {"1.1.1.1"},
+            {"1.1.1.1"},
+        ]
+        # Request a database to a non leader unit.
+        with self.harness.hooks_disabled():
+            self.harness.set_leader(False)
+        self.request_database()
+        _defer.assert_not_called()
+        _set_up_relation.assert_not_called()
 
-            # Request a database before the database is ready.
-            self.request_database()
-            _defer.assert_called_once()
+        # Request a database before the database is ready.
+        with self.harness.hooks_disabled():
+            self.harness.set_leader()
+        self.request_database()
+        _defer.assert_called_once()
+        _set_up_relation.assert_not_called()
 
-            # Request a database before primary endpoint is available.
-            self.request_database()
-            self.assertEqual(_defer.call_count, 2)
+        # Request a database before primary endpoint is available.
+        self.request_database()
+        self.assertEqual(_defer.call_count, 2)
+        _set_up_relation.assert_not_called()
 
-            # Request it again when the database is ready.
-            self.request_database()
-
-            # Assert that the correct calls were made.
-            user = f"relation-{self.rel_id}"
-            postgresql_mock.create_user.assert_called_once_with(user, "test-password", False)
-            postgresql_mock.create_database.assert_called_once_with(DATABASE, user)
-            postgresql_mock.get_postgresql_version.assert_called_once()
-            _update_endpoints.assert_called_once()
-
-            # Assert that the relation data was updated correctly.
-            self.assertEqual(
-                self.harness.get_relation_data(self.rel_id, self.unit),
-                {
-                    "database": DATABASE,
-                    "password": "test-password",
-                    "version": POSTGRESQL_VERSION,
-                },
-            )
-
-            # Assert no BlockedStatus was set.
-            self.assertFalse(isinstance(self.harness.model.unit.status, BlockedStatus))
-
-            # BlockedStatus due to a PostgreSQLCreateUserError.
-            self.request_database()
-            self.assertTrue(isinstance(self.harness.model.unit.status, BlockedStatus))
-            # No data is set in the databag by the database.
-            self.assertEqual(
-                self.harness.get_relation_data(self.rel_id, self.app),
-                {},
-            )
-
-            # BlockedStatus due to a PostgreSQLCreateDatabaseError.
-            self.request_database()
-            self.assertTrue(isinstance(self.harness.model.unit.status, BlockedStatus))
-            # No data is set in the databag by the database.
-            self.assertEqual(
-                self.harness.get_relation_data(self.rel_id, self.app),
-                {},
-            )
-
-            # BlockedStatus due to a PostgreSQLGetPostgreSQLVersionError.
-            self.request_database()
-            self.assertTrue(isinstance(self.harness.model.unit.status, BlockedStatus))
+        # Request it again when the database is ready.
+        _defer.reset_mock()
+        self.request_database()
+        _defer.assert_not_called()
+        _set_up_relation.assert_called_once()
 
     def test_get_extensions(self):
         # Test when there are no extensions in the relation databags.
@@ -226,8 +182,191 @@ class TestDbProvides(unittest.TestCase):
             ([extensions[1], extensions[2]], {extensions[2]}),
         )
 
-    def test_set_up_relation(self):
-        pass
+    @patch("relations.db.DbProvides._update_unit_status")
+    @patch("relations.db.DbProvides.update_endpoints")
+    @patch("charm.PostgresqlOperatorCharm.enable_disable_extensions")
+    @patch("relations.db.new_password", return_value="test-password")
+    @patch("relations.db.DbProvides._get_extensions")
+    def test_set_up_relation(
+        self,
+        _get_extensions,
+        _new_password,
+        _enable_disable_extensions,
+        _update_endpoints,
+        _update_unit_status,
+    ):
+        with patch.object(PostgresqlOperatorCharm, "postgresql", Mock()) as postgresql_mock:
+            # Define some mocks' side effects.
+            extensions = ["citext:public", "debversion"]
+            _get_extensions.side_effect = [
+                (extensions, {"debversion"}),
+                (extensions, set()),
+                (extensions, set()),
+                (extensions, set()),
+                (extensions, set()),
+                (extensions, set()),
+                (extensions, set()),
+            ]
+            postgresql_mock.create_user = PropertyMock(
+                side_effect=[None, None, None, PostgreSQLCreateUserError, None, None]
+            )
+            postgresql_mock.create_database = PropertyMock(
+                side_effect=[None, None, None, PostgreSQLCreateDatabaseError, None]
+            )
+            postgresql_mock.get_postgresql_version = PropertyMock(
+                side_effect=[
+                    POSTGRESQL_VERSION,
+                    POSTGRESQL_VERSION,
+                    POSTGRESQL_VERSION,
+                    PostgreSQLGetPostgreSQLVersionError,
+                ]
+            )
+
+            # Assert no operation is done when at least one of the requested extensions
+            # is disabled.
+            relation = self.harness.model.get_relation(RELATION_NAME, self.rel_id)
+            self.assertFalse(self.harness.charm.legacy_db_relation.set_up_relation(relation))
+            postgresql_mock.create_user.assert_not_called()
+            postgresql_mock.create_database.assert_not_called()
+            _enable_disable_extensions.assert_not_called()
+            postgresql_mock.get_postgresql_version.assert_not_called()
+            _update_endpoints.assert_not_called()
+            _update_unit_status.assert_not_called()
+
+            # Assert that the correct calls were made in a successful setup.
+            self.harness.charm.unit.status = ActiveStatus()
+            with self.harness.hooks_disabled():
+                self.harness.update_relation_data(
+                    self.rel_id,
+                    "application",
+                    {"database": DATABASE},
+                )
+            self.assertTrue(self.harness.charm.legacy_db_relation.set_up_relation(relation))
+            user = f"relation-{self.rel_id}"
+            postgresql_mock.create_user.assert_called_once_with(user, "test-password", False)
+            postgresql_mock.create_database.assert_called_once_with(DATABASE, user)
+            _enable_disable_extensions.assert_called_once()
+            postgresql_mock.get_postgresql_version.assert_called_once()
+            _update_endpoints.assert_called_once()
+            _update_unit_status.assert_called_once()
+            self.assertEqual(
+                self.harness.get_relation_data(self.rel_id, self.unit),
+                {
+                    "database": DATABASE,
+                    "extensions": ",".join(extensions),
+                    "password": "test-password",
+                    "version": POSTGRESQL_VERSION,
+                },
+            )
+            self.assertNotIsInstance(self.harness.model.unit.status, BlockedStatus)
+
+            # Assert that the correct calls were made when the database name is not
+            # provided in both application and unit databags.
+            postgresql_mock.create_user.reset_mock()
+            postgresql_mock.create_database.reset_mock()
+            _enable_disable_extensions.reset_mock()
+            postgresql_mock.get_postgresql_version.reset_mock()
+            _update_endpoints.reset_mock()
+            _update_unit_status.reset_mock()
+            with self.harness.hooks_disabled():
+                self.harness.update_relation_data(
+                    self.rel_id,
+                    "application",
+                    {"database": ""},
+                )
+                self.harness.update_relation_data(
+                    self.rel_id,
+                    "application/0",
+                    {"database": DATABASE},
+                )
+            self.assertTrue(self.harness.charm.legacy_db_relation.set_up_relation(relation))
+            postgresql_mock.create_user.assert_called_once_with(user, "test-password", False)
+            postgresql_mock.create_database.assert_called_once_with(DATABASE, user)
+            _enable_disable_extensions.assert_called_once()
+            postgresql_mock.get_postgresql_version.assert_called_once()
+            _update_endpoints.assert_called_once()
+            _update_unit_status.assert_called_once()
+            self.assertEqual(
+                self.harness.get_relation_data(self.rel_id, self.unit),
+                {
+                    "database": DATABASE,
+                    "extensions": ",".join(extensions),
+                    "password": "test-password",
+                    "version": POSTGRESQL_VERSION,
+                },
+            )
+            self.assertNotIsInstance(self.harness.model.unit.status, BlockedStatus)
+
+            # Assert that the correct calls were made when the database name is not provided.
+            postgresql_mock.create_user.reset_mock()
+            postgresql_mock.create_database.reset_mock()
+            _enable_disable_extensions.reset_mock()
+            postgresql_mock.get_postgresql_version.reset_mock()
+            _update_endpoints.reset_mock()
+            _update_unit_status.reset_mock()
+            with self.harness.hooks_disabled():
+                self.harness.update_relation_data(
+                    self.rel_id,
+                    "application/0",
+                    {"database": ""},
+                )
+            self.assertTrue(self.harness.charm.legacy_db_relation.set_up_relation(relation))
+            postgresql_mock.create_user.assert_called_once_with(user, "test-password", False)
+            postgresql_mock.create_database.assert_called_once_with("application", user)
+            _enable_disable_extensions.assert_called_once()
+            postgresql_mock.get_postgresql_version.assert_called_once()
+            _update_endpoints.assert_called_once()
+            _update_unit_status.assert_called_once()
+            self.assertEqual(
+                self.harness.get_relation_data(self.rel_id, self.unit),
+                {
+                    "database": "application",
+                    "extensions": ",".join(extensions),
+                    "password": "test-password",
+                    "version": POSTGRESQL_VERSION,
+                },
+            )
+            self.assertNotIsInstance(self.harness.model.unit.status, BlockedStatus)
+
+            # BlockedStatus due to a PostgreSQLCreateUserError.
+            postgresql_mock.create_database.reset_mock()
+            _enable_disable_extensions.reset_mock()
+            postgresql_mock.get_postgresql_version.reset_mock()
+            _update_endpoints.reset_mock()
+            _update_unit_status.reset_mock()
+            self.assertFalse(self.harness.charm.legacy_db_relation.set_up_relation(relation))
+            postgresql_mock.create_database.assert_not_called()
+            _enable_disable_extensions.assert_not_called()
+            postgresql_mock.get_postgresql_version.assert_not_called()
+            _update_endpoints.assert_not_called()
+            _update_unit_status.assert_not_called()
+            self.assertIsInstance(self.harness.model.unit.status, BlockedStatus)
+            # No data is set in the databag by the database.
+            self.assertEqual(
+                self.harness.get_relation_data(self.rel_id, self.app),
+                {},
+            )
+
+            # BlockedStatus due to a PostgreSQLCreateDatabaseError.
+            self.harness.charm.unit.status = ActiveStatus()
+            self.assertFalse(self.harness.charm.legacy_db_relation.set_up_relation(relation))
+            _enable_disable_extensions.assert_not_called()
+            postgresql_mock.get_postgresql_version.assert_not_called()
+            _update_endpoints.assert_not_called()
+            _update_unit_status.assert_not_called()
+            self.assertIsInstance(self.harness.model.unit.status, BlockedStatus)
+            # No data is set in the databag by the database.
+            self.assertEqual(
+                self.harness.get_relation_data(self.rel_id, self.app),
+                {},
+            )
+
+            # BlockedStatus due to a PostgreSQLGetPostgreSQLVersionError.
+            self.harness.charm.unit.status = ActiveStatus()
+            self.assertFalse(self.harness.charm.legacy_db_relation.set_up_relation(relation))
+            _update_endpoints.assert_not_called()
+            _update_unit_status.assert_not_called()
+            self.assertIsInstance(self.harness.model.unit.status, BlockedStatus)
 
     @patch("relations.db.DbProvides._check_for_blocking_relations")
     @patch("charm.PostgresqlOperatorCharm.is_blocked", new_callable=PropertyMock)
