4719c1f370b290526fb68305de9d4f35ea2ad5dd
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2023-08-21 00:50:30 -0300

    Add extra tests

--- tests/integration/ha_tests/test_upgrade.py
+++ tests/integration/ha_tests/test_upgrade.py
@@ -43,7 +43,7 @@ async def test_build_and_deploy(ops_test: OpsTest) -> None:
             await ops_test.model.wait_for_idle(status="active", timeout=1000)
 
 
-async def test_upgrade(ops_test: OpsTest, continuous_writes) -> None:
+async def test_successful_upgrade(ops_test: OpsTest, continuous_writes) -> None:
     # Start an application that continuously writes data to the database.
     logger.info("starting continuous writes to the database")
     app = await app_name(ops_test)
@@ -57,6 +57,40 @@ async def test_upgrade(ops_test: OpsTest, continuous_writes) -> None:
 
     # Run the pre-upgrade-check action.
     logger.info("running pre-upgrade check")
+    leader_unit_name = None
+    for unit in ops_test.model.applications[app].units:
+        if await unit.is_leader_from_status():
+            leader_unit_name = unit.name
+            break
+    action = await ops_test.model.units.get(leader_unit_name).run_action("pre-upgrade-check")
+    await action.wait()
+    assert action.results["Code"] == "0"
+
+    # Run juju refresh.
+    logger.info("refreshing the charm")
+    application = ops_test.model.applications[app]
+    charm = await ops_test.build_charm(".")
+    await application.refresh(path=charm)
+    async with ops_test.fast_forward(fast_interval="30s"):
+        await ops_test.model.wait_for_idle(
+            apps=[app], status="active", idle_period=15, raise_on_blocked=True
+        )
+
+    # Check whether writes are increasing.
+    logger.info("checking whether writes are increasing")
+    primary_name = await get_primary(ops_test, any_unit_name)
+    await are_writes_increasing(ops_test, primary_name)
+
+    # Verify that no writes to the database were missed after stopping the writes
+    # (check that all the units have all the writes).
+    logger.info("checking whether no writes were lost")
+    await check_writes(ops_test)
+
+
+async def test_failed_upgrade(ops_test: OpsTest) -> None:
+    # Run the pre-upgrade-check action.
+    logger.info("running pre-upgrade check")
+    app = await app_name(ops_test)
     leader_unit_name = None
     for unit in ops_test.model.applications[app].units:
         if await unit.is_leader_from_status():
@@ -106,15 +140,29 @@ async def test_upgrade(ops_test: OpsTest, continuous_writes) -> None:
         )
     async with ops_test.fast_forward(fast_interval="30s"):
         await ops_test.model.wait_for_idle(
-            apps=[app], status="active", idle_period=15, raise_on_blocked=True
+            apps=[app], status="blocked", idle_period=15, raise_on_blocked=False
         )
 
-    # Check whether writes are increasing.
-    logger.info("checking whether writes are increasing")
-    primary_name = await get_primary(ops_test, any_unit_name)
-    await are_writes_increasing(ops_test, primary_name)
 
-    # Verify that no writes to the database were missed after stopping the writes
-    # (check that all the units have all the writes).
-    logger.info("checking whether no writes were lost")
-    await check_writes(ops_test)
+async def test_rollback(ops_test: OpsTest) -> None:
+    # Run the pre-upgrade-check action.
+    logger.info("running pre-upgrade check")
+    app = await app_name(ops_test)
+    leader_unit_name = None
+    for unit in ops_test.model.applications[app].units:
+        if await unit.is_leader_from_status():
+            leader_unit_name = unit.name
+            break
+    action = await ops_test.model.units.get(leader_unit_name).run_action("pre-upgrade-check")
+    await action.wait()
+    assert action.results["Code"] == "0"
+
+    # Run juju refresh.
+    logger.info("refreshing the charm")
+    application = ops_test.model.applications[app]
+    charm = await ops_test.build_charm(".")
+    await application.refresh(path=charm)
+    async with ops_test.fast_forward(fast_interval="30s"):
+        await ops_test.model.wait_for_idle(
+            apps=[app], status="active", idle_period=15, raise_on_blocked=True
+        )
