8e3fd7984254667af1e52578485242727c470ffa
Dragomir Penev <dragomir.penev@canonical.com>
2023-11-09 13:53:47 +0200

    Bump timeouts

--- tests/integration/test_backups.py
+++ tests/integration/test_backups.py
@@ -125,7 +125,7 @@ async def test_backup(ops_test: OpsTest, cloud_configs: Tuple[Dict, Dict]) -> No
         )
         await action.wait()
         await ops_test.model.wait_for_idle(
-            apps=[database_app_name, S3_INTEGRATOR_APP_NAME], status="active", timeout=1000
+            apps=[database_app_name, S3_INTEGRATOR_APP_NAME], status="active", timeout=1500
         )
 
         primary = await get_primary(ops_test, f"{database_app_name}/0")
