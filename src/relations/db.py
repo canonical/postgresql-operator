bbbd149691e89e73bc7407b141c579ad4ed962cb
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2023-11-30 08:33:53 -0300

    Handle tables ownership (#298)
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>

--- src/relations/db.py
+++ src/relations/db.py
@@ -174,7 +174,9 @@ class DbProvides(Object):
                 if self.charm.config[plugin]
             ]
 
-            self.charm.postgresql.create_database(database, user, plugins=plugins)
+            self.charm.postgresql.create_database(
+                database, user, plugins=plugins, client_relations=self.charm.client_relations
+            )
 
         except (PostgreSQLCreateDatabaseError, PostgreSQLCreateUserError) as e:
             logger.exception(e)
--- tests/unit/test_db.py
+++ tests/unit/test_db.py
@@ -235,7 +235,9 @@ class TestDbProvides(unittest.TestCase):
             self.assertTrue(self.harness.charm.legacy_db_relation.set_up_relation(relation))
             user = f"relation-{self.rel_id}"
             postgresql_mock.create_user.assert_called_once_with(user, "test-password", False)
-            postgresql_mock.create_database.assert_called_once_with(DATABASE, user, plugins=[])
+            postgresql_mock.create_database.assert_called_once_with(
+                DATABASE, user, plugins=[], client_relations=[relation]
+            )
             _update_endpoints.assert_called_once()
             _update_unit_status.assert_called_once()
             self.assertNotIsInstance(self.harness.model.unit.status, BlockedStatus)
@@ -260,7 +262,9 @@ class TestDbProvides(unittest.TestCase):
                 )
             self.assertTrue(self.harness.charm.legacy_db_relation.set_up_relation(relation))
             postgresql_mock.create_user.assert_called_once_with(user, "test-password", False)
-            postgresql_mock.create_database.assert_called_once_with(DATABASE, user, plugins=[])
+            postgresql_mock.create_database.assert_called_once_with(
+                DATABASE, user, plugins=[], client_relations=[relation]
+            )
             _update_endpoints.assert_called_once()
             _update_unit_status.assert_called_once()
             self.assertNotIsInstance(self.harness.model.unit.status, BlockedStatus)
@@ -280,7 +284,7 @@ class TestDbProvides(unittest.TestCase):
             self.assertTrue(self.harness.charm.legacy_db_relation.set_up_relation(relation))
             postgresql_mock.create_user.assert_called_once_with(user, "test-password", False)
             postgresql_mock.create_database.assert_called_once_with(
-                "application", user, plugins=[]
+                "application", user, plugins=[], client_relations=[relation]
             )
             _update_endpoints.assert_called_once()
             _update_unit_status.assert_called_once()
