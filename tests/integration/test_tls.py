adeeb45c1439be6f856c0407abc82f2bd922e37f
Dragomir Penev <6687393+dragomirp@users.noreply.github.com>
2024-01-25 19:22:38 +0200

    [DPE-2904] Switching to self-hosted runners (#329)
    * Update data-platform-workflows to v11
    
    * Switch to latest dpw
    
    * Git add self-hosted test
    
    * Switch to self-hosted runners for all tests
    
    * Code review improvements
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- tests/integration/test_tls.py
+++ tests/integration/test_tls.py
@@ -30,6 +30,7 @@ APP_NAME = METADATA["name"]
 TLS_CERTIFICATES_APP_NAME = "tls-certificates-operator"
 
 
+@pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
 @pytest.mark.group(1)
 @pytest.mark.abort_on_fail
 @pytest.mark.skip_if_deployed
