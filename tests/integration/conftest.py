7f2846356b92035f04163611c51bee1626c3a118
Dragomir Penev <dragomir.penev@canonical.com>
2024-02-06 01:07:02 +0200

    Revert conftest

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
-import platform
-
 import pytest
 from pytest_operator.plugin import OpsTest
-
-arch_mapping = {"x86_64": "amd64", "aarch64": "arm64"}
 
 
 @pytest.fixture(scope="module")
@@ -14,8 +10,3 @@ async def charm(ops_test: OpsTest):
     """Build the charm-under-test."""
     # Build charm from local source folder.
     yield await ops_test.build_charm(".")
-
-
-@pytest.fixture(scope="module")
-def cpu_arch():
-    return arch_mapping.get(platform.machine(), platform.machine())
