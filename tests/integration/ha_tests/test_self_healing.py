64b65b4bf697cf5f6dd7e5bae873851008cfe69b
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2024-02-09 10:56:20 -0300

    [DPE-3257] Fix network cut tests (#346)
    * Fix network cut test
    
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
    
    * Fix network cut test without IP change
    
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
    
    * Update unit test
    
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
    
    * Fix retrieval of units IPs
    
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
    
    * Improve checks for readiness
    
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
    
    * Fix IP retrieval
    
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
    
    * Fix IP retrieval
    
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
    
    * Fix IP retrieval
    
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
    
    ---------
    
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>

--- tests/integration/ha_tests/test_self_healing.py
+++ tests/integration/ha_tests/test_self_healing.py
@@ -383,7 +383,6 @@ async def test_forceful_restart_without_data_and_transaction_logs(
 
 
 @pytest.mark.group(1)
-@pytest.mark.unstable
 async def test_network_cut(ops_test: OpsTest, continuous_writes, primary_start_timeout):
     """Completely cut and restore network."""
     # Locate primary unit.
@@ -456,19 +455,22 @@ async def test_network_cut(ops_test: OpsTest, continuous_writes, primary_start_t
 
     # Wait the LXD unit has its IP updated.
     logger.info("waiting for IP address to be updated on Juju unit")
-    await wait_network_restore(ops_test, primary_hostname, primary_ip)
+    await wait_network_restore(ops_test, primary_name, primary_ip)
+
+    # Verify that the database service got restarted and is ready in the old primary.
+    logger.info(f"waiting for the database service to be ready on {primary_name}")
+    assert await is_postgresql_ready(ops_test, primary_name, use_ip_from_inside=True)
 
     # Verify that connection is possible.
     logger.info("checking whether the connectivity to the database is working")
     assert await is_connection_possible(
-        ops_test, primary_name
+        ops_test, primary_name, use_ip_from_inside=True
     ), "Connection is not possible after network restore"
 
-    await is_cluster_updated(ops_test, primary_name)
+    await is_cluster_updated(ops_test, primary_name, use_ip_from_inside=True)
 
 
 @pytest.mark.group(1)
-@pytest.mark.unstable
 async def test_network_cut_without_ip_change(
     ops_test: OpsTest, continuous_writes, primary_start_timeout
 ):
@@ -516,7 +518,7 @@ async def test_network_cut_without_ip_change(
 
     async with ops_test.fast_forward():
         logger.info("checking whether writes are increasing")
-        await are_writes_increasing(ops_test, primary_name)
+        await are_writes_increasing(ops_test, primary_name, use_ip_from_inside=True)
 
         logger.info("checking whether a new primary was elected")
         # Verify that a new primary gets elected (ie old primary is secondary).
@@ -533,10 +535,14 @@ async def test_network_cut_without_ip_change(
     async with ops_test.fast_forward():
         await ops_test.model.wait_for_idle(apps=[app], status="active")
 
+    # Verify that the database service got restarted and is ready in the old primary.
+    logger.info(f"waiting for the database service to be ready on {primary_name}")
+    assert await is_postgresql_ready(ops_test, primary_name)
+
     # Verify that connection is possible.
     logger.info("checking whether the connectivity to the database is working")
     assert await is_connection_possible(
         ops_test, primary_name
     ), "Connection is not possible after network restore"
 
-    await is_cluster_updated(ops_test, primary_name)
+    await is_cluster_updated(ops_test, primary_name, use_ip_from_inside=True)
