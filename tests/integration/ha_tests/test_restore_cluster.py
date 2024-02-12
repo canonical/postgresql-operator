7163a3377e18a69ef33be250716a732f3e43505b
Dragomir Penev <dragomir.penev@canonical.com>
2024-02-12 12:29:43 +0200

    Test cluster restore failures on self-hosted runners

--- tests/integration/ha_tests/test_restore_cluster.py
+++ tests/integration/ha_tests/test_restore_cluster.py
@@ -29,7 +29,6 @@ logger = logging.getLogger(__name__)
 charm = None
 
 
-@pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
 @pytest.mark.group(1)
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest) -> None:
