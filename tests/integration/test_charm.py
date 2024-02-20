487ab8db285604e64bde5d624d4b4b9700d7a02f
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2024-02-20 20:08:05 -0300

    Increase test timeout
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -313,7 +313,7 @@ async def test_persist_data_through_primary_deletion(ops_test: OpsTest):
 
     # Add the unit again.
     await ops_test.model.applications[DATABASE_APP_NAME].add_unit(count=1)
-    await ops_test.model.wait_for_idle(apps=[DATABASE_APP_NAME], status="active", timeout=1500)
+    await ops_test.model.wait_for_idle(apps=[DATABASE_APP_NAME], status="active", timeout=2000)
 
     # Testing write occurred to every postgres instance by reading from them
     for unit in ops_test.model.applications[DATABASE_APP_NAME].units:
