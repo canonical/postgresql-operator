da261e2085feb5e6d5e4200c64eb460a1372d1ba
Dragomir Penev <dragomir.penev@canonical.com>
2024-03-06 23:02:34 +0200

    Landscape client

---
+++ tests/integration/test_subordinates.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# Copyright 2021 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import logging
+import subprocess
+from asyncio import gather
+
+import pytest
+from pytest_operator.plugin import OpsTest
+
+from .helpers import (
+    CHARM_SERIES,
+    get_unit_address,
+)
+
+DATABASE_APP_NAME = "pg"
+LS_CLIENT = "landscape-client"
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.group(1)
+@pytest.mark.abort_on_fail
+async def test_deploy(ops_test: OpsTest, charm: str, github_secrets):
+    landscape_config = {
+        "admin_email": "admin@example.com",
+        "admin_name": "Admin",
+        "admin_password": "qweqwepoipoi",
+    }
+    await gather(
+        ops_test.model.deploy(
+            charm,
+            application_name=DATABASE_APP_NAME,
+            num_units=3,
+            series=CHARM_SERIES,
+            config={"profile": "testing"},
+        ),
+        ops_test.model.deploy("landscape-scalable"),
+        ops_test.model.deploy(LS_CLIENT, num_units=0),
+    )
+    await ops_test.model.applications["landscape-server"].set_config(landscape_config)
+
+    await ops_test.model.wait_for_idle(
+        apps=["landscape-server", "haproxy", DATABASE_APP_NAME], status="active", timeout=3000
+    )
+    haproxy_unit = ops_test.model.applications["haproxy"].units[0]
+    haproxy_addr = get_unit_address(ops_test, haproxy_unit.name)
+    haproxy_host = haproxy_unit.machine.hostname
+    subprocess.check_output(
+        ["lxc", "file", "pull", f"{haproxy_host}/var/lib/haproxy/selfsigned_ca.crt", "/tmp/"]
+    )
+    for unit in ops_test.model.applications[DATABASE_APP_NAME].units:
+        subprocess.check_output(
+            [
+                "lxc",
+                "file",
+                "push",
+                "/tmp/selfsigned_ca.crt",
+                f"{unit.machine.hostname}/var/selfsigned_ca.crt",
+            ]
+        )
+        subprocess.check_output(
+            [
+                "lxc",
+                "exec",
+                unit.machine.hostname,
+                "chmod",
+                "666",
+                "/var/selfsigned_ca.crt",
+            ]
+        )
+
+    await ops_test.model.applications[LS_CLIENT].set_config(
+        {
+            "account-name": "standalone",
+            "ping-url": f"http://{haproxy_addr}/ping",
+            "url": f"https://{haproxy_addr}/message-system",
+            "ssl-public-key": "/var/selfsigned_ca.crt",
+        }
+    )
+    await ops_test.model.relate(f"{DATABASE_APP_NAME}:juju-info", LS_CLIENT)
+    await ops_test.model.wait_for_idle(
+        apps=[LS_CLIENT],
+        wait_for_at_least_units=3,
+        status="active",
+        timeout=1500,
+    )
