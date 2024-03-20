0dbfbc2b08ccd5b2879c74f5fa4ab6c99840998d
DmitriyBalaba <d.balaba@jazzserve.com>
2024-03-20 15:00:23 +0200

    test legacy + modern endpoints

--- tests/integration/relations/test_relations.py
+++ tests/integration/relations/test_relations.py
@@ -61,80 +61,80 @@ async def test_deploy_charms(ops_test: OpsTest, charm):
 
         await ops_test.model.wait_for_idle(apps=APP_NAMES, status="active", timeout=3000)
 
-@pytest.mark.group(1)
-async def test_legacy_modern_endpoints(ops_test: OpsTest):
-    await ops_test.model.relate(MAILMAN3_CORE_APP_NAME, f"{APP_NAME}:{DB_RELATION}")
-    await ops_test.model.relate(APP_NAME, f"{APPLICATION_APP_NAME}:{FIRST_DATABASE_RELATION}")
-
-    app = ops_test.model.applications[APP_NAME]
-    await ops_test.model.block_until(
-        lambda: "blocked" in {unit.workload_status for unit in app.units},
-        timeout=1500,
-    )
-
-    logger.info(f" remove relation with  modern endpoints")
-    await ops_test.model.applications[APP_NAME].remove_relation(
-             f"{APP_NAME}:{DATABASE_RELATION}", f"{APPLICATION_APP_NAME}:{FIRST_DATABASE_RELATION}"
-    )
-    async with ops_test.fast_forward():
-        await ops_test.model.wait_for_idle(
-            status="active",
-            timeout=1500,
-            raise_on_error=False,
-        )
-
-    legacy_interface_connect = await get_legacy_db_connection_str(ops_test, MAILMAN3_CORE_APP_NAME, DB_RELATION,
-                                                                  remote_unit_name=f"{APP_NAME}/0")
-    logger.info(f" check connect to = {legacy_interface_connect}")
-    for attempt in Retrying(stop=stop_after_delay(60 * 3), wait=wait_fixed(10)):
-        with attempt:
-            with psycopg2.connect(legacy_interface_connect) as connection:
-                assert connection.status == psycopg2.extensions.STATUS_READY
-
-    logger.info(f"==== remove relation mailman3-core")
-    async with ops_test.fast_forward():
-        await ops_test.model.applications[APP_NAME].remove_relation(
-            f"{APP_NAME}:db", f"mailman3-core:db"
-        )
-        await ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", timeout=1000)
-        with pytest.raises(psycopg2.OperationalError):
-            psycopg2.connect(legacy_interface_connect)
-
-async def test_legacy_modern_endpoints_a(ops_test: OpsTest):
-    await ops_test.model.relate(MAILMAN3_CORE_APP_NAME, f"{APP_NAME}:{DB_RELATION}")
-    await ops_test.model.relate(APP_NAME, f"{APPLICATION_APP_NAME}:{FIRST_DATABASE_RELATION}")
-
-    app = ops_test.model.applications[APP_NAME]
-    await ops_test.model.block_until(
-        lambda: "blocked" in {unit.workload_status for unit in app.units},
-        timeout=1500,
-    )
-
-    logger.info(f" remove relation with legacy endpoints")
-    await ops_test.model.applications[APP_NAME].remove_relation(
-        f"{MAILMAN3_CORE_APP_NAME}:{DB_RELATION}", f"{APP_NAME}:{DB_RELATION}"
-    )
-    async with ops_test.fast_forward():
-        await ops_test.model.wait_for_idle(
-            status="active",
-            timeout=3000,
-            raise_on_error=False
-        )
-
-    modern_interface_connect = await build_connection_string(ops_test, APPLICATION_APP_NAME, FIRST_DATABASE_RELATION)
-    logger.info(f"  ====================  modern database_unit_name  ={modern_interface_connect}")
-    for attempt in Retrying(stop=stop_after_delay(60 * 3), wait=wait_fixed(10)):
-        with attempt:
-            with psycopg2.connect(modern_interface_connect) as connection:
-                assert connection.status == psycopg2.extensions.STATUS_READY
-
-    logger.info(f"==== modern remove relation {APPLICATION_APP_NAME}")
-    async with ops_test.fast_forward():
-        await ops_test.model.applications[APP_NAME].remove_relation(
-            f"{APP_NAME}:{DATABASE_RELATION}", f"{APPLICATION_APP_NAME}:{FIRST_DATABASE_RELATION}"
-        )
-        await ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", timeout=1000)
-        for attempt in Retrying(stop=stop_after_delay(60 * 5), wait=wait_fixed(10)):
-            with attempt:
-                with pytest.raises(psycopg2.OperationalError):
-                    psycopg2.connect(modern_interface_connect)
+# @pytest.mark.group(1)
+# async def test_legacy_modern_endpoints(ops_test: OpsTest):
+#     await ops_test.model.relate(MAILMAN3_CORE_APP_NAME, f"{APP_NAME}:{DB_RELATION}")
+#     await ops_test.model.relate(APP_NAME, f"{APPLICATION_APP_NAME}:{FIRST_DATABASE_RELATION}")
+#
+#     app = ops_test.model.applications[APP_NAME]
+#     await ops_test.model.block_until(
+#         lambda: "blocked" in {unit.workload_status for unit in app.units},
+#         timeout=1500,
+#     )
+#
+#     logger.info(f" remove relation with  modern endpoints")
+#     await ops_test.model.applications[APP_NAME].remove_relation(
+#              f"{APP_NAME}:{DATABASE_RELATION}", f"{APPLICATION_APP_NAME}:{FIRST_DATABASE_RELATION}"
+#     )
+#     async with ops_test.fast_forward():
+#         await ops_test.model.wait_for_idle(
+#             status="active",
+#             timeout=1500,
+#             raise_on_error=False,
+#         )
+#
+#     legacy_interface_connect = await get_legacy_db_connection_str(ops_test, MAILMAN3_CORE_APP_NAME, DB_RELATION,
+#                                                                   remote_unit_name=f"{APP_NAME}/0")
+#     logger.info(f" check connect to = {legacy_interface_connect}")
+#     for attempt in Retrying(stop=stop_after_delay(60 * 3), wait=wait_fixed(10)):
+#         with attempt:
+#             with psycopg2.connect(legacy_interface_connect) as connection:
+#                 assert connection.status == psycopg2.extensions.STATUS_READY
+#
+#     logger.info(f"==== remove relation mailman3-core")
+#     async with ops_test.fast_forward():
+#         await ops_test.model.applications[APP_NAME].remove_relation(
+#             f"{APP_NAME}:db", f"mailman3-core:db"
+#         )
+#         await ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", timeout=1000)
+#         with pytest.raises(psycopg2.OperationalError):
+#             psycopg2.connect(legacy_interface_connect)
+#
+# async def test_legacy_modern_endpoints_a(ops_test: OpsTest):
+#     await ops_test.model.relate(MAILMAN3_CORE_APP_NAME, f"{APP_NAME}:{DB_RELATION}")
+#     await ops_test.model.relate(APP_NAME, f"{APPLICATION_APP_NAME}:{FIRST_DATABASE_RELATION}")
+#
+#     app = ops_test.model.applications[APP_NAME]
+#     await ops_test.model.block_until(
+#         lambda: "blocked" in {unit.workload_status for unit in app.units},
+#         timeout=1500,
+#     )
+#
+#     logger.info(f" remove relation with legacy endpoints")
+#     await ops_test.model.applications[APP_NAME].remove_relation(
+#         f"{MAILMAN3_CORE_APP_NAME}:{DB_RELATION}", f"{APP_NAME}:{DB_RELATION}"
+#     )
+#     async with ops_test.fast_forward():
+#         await ops_test.model.wait_for_idle(
+#             status="active",
+#             timeout=3000,
+#             raise_on_error=False
+#         )
+#
+#     modern_interface_connect = await build_connection_string(ops_test, APPLICATION_APP_NAME, FIRST_DATABASE_RELATION)
+#     logger.info(f"  ====================  modern database_unit_name  ={modern_interface_connect}")
+#     for attempt in Retrying(stop=stop_after_delay(60 * 3), wait=wait_fixed(10)):
+#         with attempt:
+#             with psycopg2.connect(modern_interface_connect) as connection:
+#                 assert connection.status == psycopg2.extensions.STATUS_READY
+#
+#     logger.info(f"==== modern remove relation {APPLICATION_APP_NAME}")
+#     async with ops_test.fast_forward():
+#         await ops_test.model.applications[APP_NAME].remove_relation(
+#             f"{APP_NAME}:{DATABASE_RELATION}", f"{APPLICATION_APP_NAME}:{FIRST_DATABASE_RELATION}"
+#         )
+#         await ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", timeout=1000)
+#         for attempt in Retrying(stop=stop_after_delay(60 * 5), wait=wait_fixed(10)):
+#             with attempt:
+#                 with pytest.raises(psycopg2.OperationalError):
+#                     psycopg2.connect(modern_interface_connect)
