b04fe1610abf00db980fd0f66fbd9db3679d92e1
DmitriyBalaba <d.balaba@jazzserve.com>
2024-04-16 22:02:21 +0300

    test_self_healing: deploying postgresql-test-app revision 101

--- tests/integration/ha_tests/test_self_healing.py
+++ tests/integration/ha_tests/test_self_healing.py
@@ -86,6 +86,7 @@ async def test_build_and_deploy(ops_test: OpsTest) -> None:
                 APPLICATION_NAME,
                 application_name=APPLICATION_NAME,
                 series=CHARM_SERIES,
+                revision=101,
                 channel="edge",
             )
 
