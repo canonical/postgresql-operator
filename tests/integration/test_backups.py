ab4121223b5c8c4b3a1e568c64cf5b1cb6540309
Vladyslav Tarasenko <vladislavstargate@gmail.com>
2024-04-22 10:48:47 +0300

    Fix PITR integration test.

--- tests/integration/test_backups.py
+++ tests/integration/test_backups.py
@@ -30,6 +30,7 @@ FAILED_TO_ACCESS_CREATE_BUCKET_ERROR_MESSAGE = (
 )
 FAILED_TO_INITIALIZE_STANZA_ERROR_MESSAGE = "failed to initialize stanza, check your S3 settings"
 CANNOT_RESTORE_PITR = "cannot restore PITR, juju debug-log for details"
+MOVE_RESTORED_CLUSTER_TO_ANOTHER_BUCKET = "Move restored cluster to another S3 bucket"
 S3_INTEGRATOR_APP_NAME = "s3-integrator"
 if juju_major_version < 3:
     TLS_CERTIFICATES_APP_NAME = "tls-certificates-operator"
@@ -283,103 +284,9 @@ async def test_backup(ops_test: OpsTest, cloud_configs: Tuple[Dict, Dict], charm
 
 
 @pytest.mark.group(1)
-async def test_restore_on_new_cluster(ops_test: OpsTest, github_secrets, charm) -> None:
-    """Test that is possible to restore a backup to another PostgreSQL cluster."""
-    previous_database_app_name = f"{DATABASE_APP_NAME}-gcp"
-    database_app_name = f"new-{DATABASE_APP_NAME}"
-    await ops_test.model.deploy(charm, application_name=previous_database_app_name)
-    await ops_test.model.deploy(
-        charm,
-        application_name=database_app_name,
-        series=CHARM_SERIES,
-    )
-    await ops_test.model.relate(previous_database_app_name, S3_INTEGRATOR_APP_NAME)
-    await ops_test.model.relate(database_app_name, S3_INTEGRATOR_APP_NAME)
-    async with ops_test.fast_forward():
-        logger.info(
-            "waiting for the database charm to become blocked due to existing backups from another cluster in the repository"
-        )
-        await wait_for_idle_on_blocked(
-            ops_test,
-            previous_database_app_name,
-            2,
-            S3_INTEGRATOR_APP_NAME,
-            ANOTHER_CLUSTER_REPOSITORY_ERROR_MESSAGE,
-        )
-        logger.info(
-            "waiting for the database charm to become blocked due to existing backups from another cluster in the repository"
-        )
-        await wait_for_idle_on_blocked(
-            ops_test,
-            database_app_name,
-            0,
-            S3_INTEGRATOR_APP_NAME,
-            ANOTHER_CLUSTER_REPOSITORY_ERROR_MESSAGE,
-        )
-
-    # Remove the database app with the same name as the previous one (that was used only to test
-    # that the cluster becomes blocked).
-    await ops_test.model.remove_application(previous_database_app_name, block_until_done=True)
-
-    # Run the "list backups" action.
-    unit_name = f"{database_app_name}/0"
-    logger.info("listing the available backups")
-    action = await ops_test.model.units.get(unit_name).run_action("list-backups")
-    await action.wait()
-    backups = action.results.get("backups")
-    assert backups, "backups not outputted"
-    await wait_for_idle_on_blocked(
-        ops_test,
-        database_app_name,
-        0,
-        S3_INTEGRATOR_APP_NAME,
-        ANOTHER_CLUSTER_REPOSITORY_ERROR_MESSAGE,
-    )
-
-    # Run the "restore backup" action.
-    for attempt in Retrying(
-        stop=stop_after_attempt(10), wait=wait_exponential(multiplier=1, min=2, max=30)
-    ):
-        with attempt:
-            logger.info("restoring the backup")
-            most_recent_backup = backups.split("\n")[-1]
-            backup_id = most_recent_backup.split()[0]
-            action = await ops_test.model.units.get(unit_name).run_action(
-                "restore", **{"backup-id": backup_id}
-            )
-            await action.wait()
-            restore_status = action.results.get("restore-status")
-            assert restore_status, "restore hasn't succeeded"
-
-    # Wait for the restore to complete.
-    async with ops_test.fast_forward():
-        unit = ops_test.model.units.get(f"{database_app_name}/0")
-        await ops_test.model.block_until(
-            lambda: unit.workload_status_message == ANOTHER_CLUSTER_REPOSITORY_ERROR_MESSAGE
-        )
-
-    # Check that the backup was correctly restored by having only the first created table.
-    logger.info("checking that the backup was correctly restored")
-    password = await get_password(ops_test, unit_name)
-    address = get_unit_address(ops_test, unit_name)
-    with db_connect(host=address, password=password) as connection, connection.cursor() as cursor:
-        cursor.execute(
-            "SELECT EXISTS (SELECT FROM information_schema.tables"
-            " WHERE table_schema = 'public' AND table_name = 'backup_table_1');"
-        )
-        assert cursor.fetchone()[
-            0
-        ], "backup wasn't correctly restored: table 'backup_table_1' doesn't exist"
-    connection.close()
-
-
-@pytest.mark.group(1)
 @pytest.mark.abort_on_fail
-async def test_pitr_backup(ops_test: OpsTest, cloud_configs: Tuple[Dict, Dict]) -> None:
+async def test_pitr_backup(ops_test: OpsTest, cloud_configs: Tuple[Dict, Dict], charm) -> None:
     """Build, deploy two units of PostgreSQL and do backup. Then, write new data into DB, switch WAL file and test point-in-time-recovery restore action."""
-    # Build the PostgreSQL charm.
-    charm = await ops_test.build_charm(".")
-
     # Deploy S3 Integrator and TLS Certificates Operator.
     await ops_test.model.deploy(S3_INTEGRATOR_APP_NAME)
     await ops_test.model.deploy(TLS_CERTIFICATES_APP_NAME, config=TLS_CONFIG, channel=TLS_CHANNEL)
@@ -531,7 +438,11 @@ async def test_pitr_backup(ops_test: OpsTest, cloud_configs: Tuple[Dict, Dict]) 
 
         # Wait for the restore to complete.
         async with ops_test.fast_forward():
-            await ops_test.model.wait_for_idle(status="active", timeout=1000)
+            await ops_test.model.block_until(
+                lambda: remaining_unit.workload_status_message
+                == MOVE_RESTORED_CLUSTER_TO_ANOTHER_BUCKET,
+                timeout=1000,
+            )
 
         # Check that the backup was correctly restored.
         primary = await get_primary(ops_test, remaining_unit.name)
@@ -560,65 +471,101 @@ async def test_pitr_backup(ops_test: OpsTest, cloud_configs: Tuple[Dict, Dict]) 
             ], "backup wasn't correctly restored: table 'backup_table_2' exists"
         connection.close()
 
-        # Run the following steps only in one cloud (it's enough for those checks).
-        if cloud == list(cloud_configs[0].keys())[0]:
-            # Remove the relation to the TLS certificates operator.
-            await ops_test.model.applications[database_app_name].remove_relation(
-                f"{database_app_name}:certificates", f"{TLS_CERTIFICATES_APP_NAME}:certificates"
-            )
-            await ops_test.model.wait_for_idle(
-                apps=[database_app_name], status="active", timeout=1000
-            )
-
-            # Scale up to be able to test primary and leader being different.
-            async with ops_test.fast_forward():
-                await scale_application(ops_test, database_app_name, 2)
-
-            # Ensure replication is working correctly.
-            new_unit_name = f"{database_app_name}/2"
-            address = get_unit_address(ops_test, new_unit_name)
-            with db_connect(
-                host=address, password=password
-            ) as connection, connection.cursor() as cursor:
-                cursor.execute(
-                    "SELECT EXISTS (SELECT FROM information_schema.tables"
-                    " WHERE table_schema = 'public' AND table_name = 'backup_table_1');"
-                )
-                assert cursor.fetchone()[
-                    0
-                ], f"replication isn't working correctly: table 'backup_table_1' doesn't exist in {new_unit_name}"
-                cursor.execute(
-                    "SELECT EXISTS (SELECT FROM information_schema.tables"
-                    " WHERE table_schema = 'public' AND table_name = 'backup_table_2');"
-                )
-                assert not cursor.fetchone()[
-                    0
-                ], f"replication isn't working correctly: table 'backup_table_2' exists in {new_unit_name}"
-            connection.close()
-
-            switchover(ops_test, primary, new_unit_name)
-
-            # Get the new primary unit.
-            primary = await get_primary(ops_test, new_unit_name)
-            # Check that the primary changed.
-            for attempt in Retrying(
-                stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=2, max=30)
-            ):
-                with attempt:
-                    assert primary == new_unit_name
-
-            # Ensure stanza is working correctly.
-            logger.info("listing the available backups")
-            action = await ops_test.model.units.get(new_unit_name).run_action("list-backups")
-            await action.wait()
-            backups = action.results.get("backups")
-            assert backups, "backups not outputted"
-            await ops_test.model.wait_for_idle(status="active", timeout=1000)
-
         # Remove the database app.
         await ops_test.model.remove_application(database_app_name, block_until_done=True)
     # Remove the TLS operator.
     await ops_test.model.remove_application(TLS_CERTIFICATES_APP_NAME, block_until_done=True)
+
+
+@pytest.mark.group(1)
+async def test_restore_on_new_cluster(ops_test: OpsTest, github_secrets, charm) -> None:
+    """Test that is possible to restore a backup to another PostgreSQL cluster."""
+    previous_database_app_name = f"{DATABASE_APP_NAME}-gcp"
+    database_app_name = f"new-{DATABASE_APP_NAME}"
+    await ops_test.model.deploy(charm, application_name=previous_database_app_name)
+    await ops_test.model.deploy(
+        charm,
+        application_name=database_app_name,
+        series=CHARM_SERIES,
+    )
+    await ops_test.model.relate(previous_database_app_name, S3_INTEGRATOR_APP_NAME)
+    await ops_test.model.relate(database_app_name, S3_INTEGRATOR_APP_NAME)
+    async with ops_test.fast_forward():
+        logger.info(
+            "waiting for the database charm to become blocked due to existing backups from another cluster in the repository"
+        )
+        await wait_for_idle_on_blocked(
+            ops_test,
+            previous_database_app_name,
+            2,
+            S3_INTEGRATOR_APP_NAME,
+            ANOTHER_CLUSTER_REPOSITORY_ERROR_MESSAGE,
+        )
+        logger.info(
+            "waiting for the database charm to become blocked due to existing backups from another cluster in the repository"
+        )
+        await wait_for_idle_on_blocked(
+            ops_test,
+            database_app_name,
+            0,
+            S3_INTEGRATOR_APP_NAME,
+            ANOTHER_CLUSTER_REPOSITORY_ERROR_MESSAGE,
+        )
+
+    # Remove the database app with the same name as the previous one (that was used only to test
+    # that the cluster becomes blocked).
+    await ops_test.model.remove_application(previous_database_app_name, block_until_done=True)
+
+    # Run the "list backups" action.
+    unit_name = f"{database_app_name}/0"
+    logger.info("listing the available backups")
+    action = await ops_test.model.units.get(unit_name).run_action("list-backups")
+    await action.wait()
+    backups = action.results.get("backups")
+    assert backups, "backups not outputted"
+    await wait_for_idle_on_blocked(
+        ops_test,
+        database_app_name,
+        0,
+        S3_INTEGRATOR_APP_NAME,
+        ANOTHER_CLUSTER_REPOSITORY_ERROR_MESSAGE,
+    )
+
+    # Run the "restore backup" action.
+    for attempt in Retrying(
+        stop=stop_after_attempt(10), wait=wait_exponential(multiplier=1, min=2, max=30)
+    ):
+        with attempt:
+            logger.info("restoring the backup")
+            most_recent_backup = backups.split("\n")[-1]
+            backup_id = most_recent_backup.split()[0]
+            action = await ops_test.model.units.get(unit_name).run_action(
+                "restore", **{"backup-id": backup_id}
+            )
+            await action.wait()
+            restore_status = action.results.get("restore-status")
+            assert restore_status, "restore hasn't succeeded"
+
+    # Wait for the restore to complete.
+    async with ops_test.fast_forward():
+        unit = ops_test.model.units.get(f"{database_app_name}/0")
+        await ops_test.model.block_until(
+            lambda: unit.workload_status_message == ANOTHER_CLUSTER_REPOSITORY_ERROR_MESSAGE
+        )
+
+    # Check that the backup was correctly restored by having only the first created table.
+    logger.info("checking that the backup was correctly restored")
+    password = await get_password(ops_test, unit_name)
+    address = get_unit_address(ops_test, unit_name)
+    with db_connect(host=address, password=password) as connection, connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT EXISTS (SELECT FROM information_schema.tables"
+            " WHERE table_schema = 'public' AND table_name = 'backup_table_1');"
+        )
+        assert cursor.fetchone()[
+            0
+        ], "backup wasn't correctly restored: table 'backup_table_1' doesn't exist"
+    connection.close()
 
 
 @pytest.mark.group(1)
