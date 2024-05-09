042a82cdd66252007dea0c37240c705cd78e6074
Artem Yevsiukov <a.yevsiukov@gmail.com>
2024-05-09 09:34:04 +0300

    Updated

--- tests/integration/new_relations/test_relations_coherence.py
+++ tests/integration/new_relations/test_relations_coherence.py
@@ -9,9 +9,15 @@ import string
 import psycopg2
 import pytest
 from pytest_operator.plugin import OpsTest
+from tenacity import Retrying, stop_after_delay, wait_fixed
 
+from ..ha_tests.helpers import (
+    remove_unit_force,
+    storage_id, is_storage_exists, add_unit_with_storage, is_postgresql_ready, get_any_deatached_storage
+)
 from ..helpers import CHARM_SERIES, DATABASE_APP_NAME
 from .helpers import build_connection_string
+from ..juju_ import juju_major_version
 
 logger = logging.getLogger(__name__)
 
@@ -31,7 +37,6 @@ async def test_deploy_charms(ops_test: OpsTest, charm):
                 application_name=APPLICATION_APP_NAME,
                 num_units=1,
                 series=CHARM_SERIES,
-                channel="edge",
             ),
             ops_test.model.deploy(
                 charm,
@@ -50,6 +55,12 @@ async def test_deploy_charms(ops_test: OpsTest, charm):
 async def test_relations(ops_test: OpsTest, charm):
     """Test that check relation data."""
     async with ops_test.fast_forward():
+        await asyncio.gather(
+
+        )
+
+        await ops_test.model.wait_for_idle(apps=APP_NAMES, status="active", timeout=3000)
+
         # Relate with client-app, wait-for-ready, check relation data, remove-relation
         await ops_test.model.add_relation(
             f"{APPLICATION_APP_NAME}:{FIRST_DATABASE_RELATION_NAME}", DATABASE_APP_NAME
@@ -79,6 +90,27 @@ async def test_relations(ops_test: OpsTest, charm):
         )
         await ops_test.model.wait_for_idle(apps=[DATABASE_APP_NAME], status="active", timeout=1000)
 
+        primary_name = ops_test.model.applications[DATABASE_APP_NAME].units[0].name
+        storage_id_str = storage_id(ops_test, primary_name)
+
+        if juju_major_version == 2:
+            await remove_unit_force(ops_test, primary_name)
+        else:
+            await ops_test.model.destroy_unit(
+                primary_name, force=True, destroy_storage=False, max_wait=1500
+            )
+
+        for attempt in Retrying(stop=stop_after_delay(15 * 3), wait=wait_fixed(3), reraise=True):
+            with attempt:
+                assert await is_storage_exists(ops_test, storage_id_str)
+
+        garbage_storage = None
+        for attempt in Retrying(stop=stop_after_delay(30 * 3), wait=wait_fixed(3), reraise=True):
+            with attempt:
+                garbage_storage = await get_any_deatached_storage(ops_test)
+
+        await add_unit_with_storage(ops_test, DATABASE_APP_NAME, garbage_storage)
+
         # Relate with client-app 2nd time, wait-for-ready, check relation data
         await ops_test.model.add_relation(
             f"{APPLICATION_APP_NAME}:{FIRST_DATABASE_RELATION_NAME}", DATABASE_APP_NAME
@@ -93,6 +125,7 @@ async def test_relations(ops_test: OpsTest, charm):
             with connection.cursor() as cursor:
                 cursor.execute("SELECT data FROM test;")
                 data = cursor.fetchone()
+                logger.info("verifying some data exist")
                 assert data[0] == "some data"
         connection.close()
 
@@ -118,10 +151,18 @@ async def test_relations(ops_test: OpsTest, charm):
         primary_connection_string = await build_connection_string(
             ops_test, APPLICATION_APP_NAME, FIRST_DATABASE_RELATION_NAME
         )
-        connection = psycopg2.connect(primary_connection_string)
-        connection.autocommit = True
-        cursor = connection.cursor()
-        random_name = f"test_{''.join(secrets.choice(string.ascii_lowercase) for _ in range(10))}"
-        cursor.execute(f"CREATE DATABASE {random_name};")
-        cursor.execute(f"DROP DATABASE {random_name};")
-        connection.close()
+
+        try:
+            connection = psycopg2.connect(primary_connection_string)
+            connection.autocommit = True
+            cursor = connection.cursor()
+            random_name = f"test_{''.join(secrets.choice(string.ascii_lowercase) for _ in range(10))}"
+            cursor.execute(f"CREATE DATABASE {random_name};")
+            cursor.execute(f"DROP DATABASE {random_name};")
+        except psycopg2.errors.InsufficientPrivilege as e:
+            assert (
+                False
+            ), "failed to connect to or run a statement in the following database"
+        finally:
+            if connection is not None:
+                connection.close()
