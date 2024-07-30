d2ddc5207f14b4d2d41f599fe667f101a7dacbaf
Dragomir Penev <dragomir.penev@canonical.com>
2024-07-30 10:55:57 +0300

    Edge test app for async replication

--- tests/integration/ha_tests/test_async_replication.py
+++ tests/integration/ha_tests/test_async_replication.py
@@ -132,8 +132,8 @@ async def test_deploy_async_replication_setup(
             num_units=CLUSTER_SIZE,
             config={"profile": "testing"},
         )
-    await ops_test.model.deploy(APPLICATION_NAME, num_units=1)
-    await second_model.deploy(APPLICATION_NAME, num_units=1)
+    await ops_test.model.deploy(APPLICATION_NAME, channel="latest/edge", num_units=1)
+    await second_model.deploy(APPLICATION_NAME, channel="latest/edge", num_units=1)
 
     async with ops_test.fast_forward(), fast_forward(second_model):
         await gather(
