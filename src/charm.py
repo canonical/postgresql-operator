291fa888be6e7c6d3b9f8bcfb889838173cd0c09
Dragomir Penev <dragomir.penev@canonical.com>
2024-09-29 21:40:55 +0300

    Try to start on rejoin via existing hooks

--- src/charm.py
+++ src/charm.py
@@ -633,8 +633,6 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
             logger.info("Cleaning up raft unit data")
             self.unit_peer_data.pop("raft_primary", None)
             self.unit_peer_data.pop("raft_stopped", None)
-            self.update_config()
-            self._patroni.start_patroni()
 
             if self.unit.is_leader():
                 self._stuck_raft_cluster_cleanup()
