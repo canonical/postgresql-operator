55d47201798c06d2d9967046790f4606cdaa85ea
Paulo Machado <paulo.machado@canonical.com>
2024-10-15 09:34:08 -0300

    tf module on charm (#643)

---
+++ terraform/outputs.tf
@@ -0,0 +1,18 @@
+output "application_name" {
+  value = juju_application.machine_postgresql.name
+}
+
+
+output "provides" {
+  value = {
+    database  = "database",
+    cos_agent = "cos-agent",
+  }
+}
+
+output "requires" {
+  value = {
+    certificates  = "certificates"
+    s3_parameters = "s3-parameters"
+  }
+}
