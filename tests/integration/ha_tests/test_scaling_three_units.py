5d972aca654169f1009181c6d8d24530616e3218
Dragomir Penev <dragomir.penev@canonical.com>
2024-10-27 18:37:41 +0200

    Parameterised test

--- tests/integration/ha_tests/test_scaling_three_units.py
+++ tests/integration/ha_tests/test_scaling_three_units.py
@@ -20,7 +20,6 @@ from .helpers import (
     are_writes_increasing,
     check_writes,
     get_cluster_roles,
-    get_primary,
     start_continuous_writes,
 )
 
@@ -59,23 +58,25 @@ async def test_build_and_deploy(ops_test: OpsTest) -> None:
 
 @pytest.mark.group(1)
 @markers.juju3
+@pytest.mark.parametrize("role", ["primaries", "sync_standbys", "replicas"])
 @pytest.mark.abort_on_fail
-async def test_removing_primary(ops_test: OpsTest, continuous_writes) -> None:
+async def test_removing_single_unit(ops_test: OpsTest, role: str, continuous_writes) -> None:
     # Start an application that continuously writes data to the database.
     app = await app_name(ops_test)
     original_roles = await get_cluster_roles(
         ops_test, ops_test.model.applications[DATABASE_APP_NAME].units[0].name
     )
     await start_continuous_writes(ops_test, app)
-    logger.info("Deleting primary")
-    primary = await get_primary(ops_test, app)
-    await stop_machine(ops_test, await get_machine_from_unit(ops_test, primary))
+    logger.info("Stopping unit")
+    unit = original_roles[role][0]
+    await stop_machine(ops_test, await get_machine_from_unit(ops_test, unit))
     await sleep(15)
-    await ops_test.model.destroy_unit(primary, force=True, destroy_storage=False, max_wait=1500)
+    logger.info("Deleting unit")
+    await ops_test.model.destroy_unit(unit, force=True, destroy_storage=False, max_wait=1500)
 
     await ops_test.model.wait_for_idle(status="active", timeout=600, idle_period=45)
 
-    await are_writes_increasing(ops_test, primary)
+    await are_writes_increasing(ops_test, unit)
 
     logger.info("Scaling back up")
     await ops_test.model.applications[DATABASE_APP_NAME].add_unit(count=1)
@@ -87,6 +88,9 @@ async def test_removing_primary(ops_test: OpsTest, continuous_writes) -> None:
     assert len(new_roles["primaries"]) == 1
     assert len(new_roles["sync_standbys"]) == 1
     assert len(new_roles["replicas"]) == 1
-    assert new_roles["primaries"][0] == original_roles["sync_standbys"][0]
+    if role == "primaries":
+        assert new_roles["primaries"][0] == original_roles["sync_standbys"][0]
+    else:
+        assert new_roles["primaries"][0] == original_roles["primaries"][0]
 
     await check_writes(ops_test)
