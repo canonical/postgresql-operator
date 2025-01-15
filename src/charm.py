27b0e7349110f32e441bec34c1041a1bd1f8679a
Dragomir Penev <dragomir.penev@canonical.com>
2025-01-15 11:48:12 +0200

    Pop the translated peer data directly

--- src/charm.py
+++ src/charm.py
@@ -240,6 +240,13 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
 
         return {self.unit, *self._peers.units}
 
+    def scoped_peer_data(self, scope: Scopes) -> dict | None:
+        """Returns peer data based on scope."""
+        if scope == APP_SCOPE:
+            return self.app_peer_data
+        elif scope == UNIT_SCOPE:
+            return self.unit_peer_data
+
     @property
     def app_peer_data(self) -> dict:
         """Application peer relation data object."""
@@ -318,8 +325,7 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
             return None
         secret_key = self._translate_field_to_secret_key(key)
         # Old translation in databag is to be deleted
-        if self.peer_relation_data(scope).fetch_my_relation_field(peers.id, key):
-            self.peer_relation_data(scope).delete_relation_data(peers.id, [key])
+        self.scoped_peer_data(scope).pop(key, None)
         self.peer_relation_data(scope).set_secret(peers.id, secret_key, value)
 
     def remove_secret(self, scope: Scopes, key: str) -> None:
