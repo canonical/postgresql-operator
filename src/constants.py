2d7df6ff3ee6edca8ce2d3118f2dda613911e318
Dragomir Penev <dragomir.penev@canonical.com>
2025-03-11 17:15:31 +0200

    Bump snaps

--- src/constants.py
+++ src/constants.py
@@ -35,7 +35,7 @@ POSTGRESQL_SNAP_NAME = "charmed-postgresql"
 SNAP_PACKAGES = [
     (
         POSTGRESQL_SNAP_NAME,
-        {"revision": {"aarch64": "154", "x86_64": "153"}},
+        {"revision": {"aarch64": "156", "x86_64": "155"}},
     )
 ]
 
