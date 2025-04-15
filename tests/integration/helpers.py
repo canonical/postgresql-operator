04e558c7e31895078f4fcc84c77ba9d5f15dcc17
reneradoi <rene.radoi@canonical.com>
2025-04-15 18:27:57 +0200

    fix integration test helpers

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -1006,7 +1006,9 @@ async def set_password(
             SYSTEM_USERS_PASSWORD_CONFIG: secret_id
         })
     except Exception:
-        await ops_test.model.update_secret(name=secret_name, data_args=[f"{username}={password}"])
+        await ops_test.model.update_secret(
+            name=secret_name, data_args=[f"{username}={password}"], new_name=secret_name
+        )
 
 
 async def start_machine(ops_test: OpsTest, machine_name: str) -> None:
