9a41525b5e308eb04798ffef995b8e754805dc06
Dragomir Penev <6687393+dragomirp@users.noreply.github.com>
2025-04-16 12:41:27 +0300

    [MISC] Sync up changes from 14 (#834)
    * [MISC] Use latest/stable lxd (#804)
    
    * Use latest stable lxd
    
    * Test tweaks
    
    * Test tweaks
    
    * Update canonical/data-platform-workflows action to v31.0.1 (#805)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * [DPE-6874] Poll all members in the cluster topology script (#810)
    
    * Poll all members in the cluster topology script
    
    * Dual branch config
    
    * Unit tests and bugfixes
    
    * Add peers when starting the observer
    
    * Retry sync up checks
    
    * [DPE-6572] Add wal_keep_size config option (#799)
    
    * Add wal_keep_size config option
    
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
    
    * Remove parameter addition
    
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
    
    * Reset durability_wal_keep_size value to PG default
    
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
    
    ---------
    
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
    
    * Create pull_request_template.md (#814)
    
    * Create SECURITY.md (#822)
    
    * Update README file's security section (#827)
    
    * Refactor headings for syntax best practice
    
    * Update the Security section
    
    * Sync docs from Discourse (#796)
    
    Co-authored-by: GitHub Actions <41898282+github-actions[bot]@users.noreply.github.com>
    
    * [MISC] Conditional checksum calculation (#812)
    
    * Bump boto
    
    * Conditional checksum calculation
    
    * [DPE-6218] Static code analysis (#828)
    
    * Create tiobe_scan.yaml
    
    * Remove push trigger
    
    * Bump snap revisions
    
    * [MISC] Disable landscape subordinate test lxd (#831)
    
    * Set series for ubuntu-advantage test and disable the landscape test
    
    * Revert to LTS LXD
    
    ---------
    
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
    Co-authored-by: Andreia <andreia.velasco@canonical.com>
    Co-authored-by: Vladimir Izmalkov <48120135+izmalk@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/constants.py
+++ src/constants.py
@@ -32,7 +32,7 @@ POSTGRESQL_SNAP_NAME = "charmed-postgresql"
 SNAP_PACKAGES = [
     (
         POSTGRESQL_SNAP_NAME,
-        {"revision": {"aarch64": "160", "x86_64": "161"}},
+        {"revision": {"aarch64": "169", "x86_64": "170"}},
     )
 ]
 
