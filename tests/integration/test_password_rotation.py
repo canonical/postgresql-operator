12a81040eb2fc1a56a33714620fd8f08057d63f3
reneradoi <rene.radoi@canonical.com>
2025-04-16 10:32:26 +0200

    remove integration tests that no longer make sense

--- tests/integration/test_password_rotation.py
+++ tests/integration/test_password_rotation.py
@@ -95,40 +95,6 @@ async def test_password_rotation(ops_test: OpsTest):
             assert check_patroni(ops_test, unit.name, restart_time)
 
 
-@markers.juju_secrets
-async def test_password_from_secret_same_as_cli(ops_test: OpsTest):
-    """Checking if password is same as returned by CLI.
-
-    I.e. we're manipulating the secret we think we're manipulating.
-    """
-    #
-    # No way to retrieve a secet by label for now (https://bugs.launchpad.net/juju/+bug/2037104)
-    # Therefore we take advantage of the fact, that we only have ONE single secret a this point
-    # So we take the single member of the list
-    # NOTE: This would BREAK if for instance units had secrets at the start...
-    #
-    password = await get_password(ops_test, username="replication")
-    complete_command = "list-secrets"
-    _, stdout, _ = await ops_test.juju(*complete_command.split())
-    secret_id = stdout.split("\n")[1].split(" ")[0]
-
-    # Getting back the pw from juju CLI
-    complete_command = f"show-secret {secret_id} --reveal --format=json"
-    _, stdout, _ = await ops_test.juju(*complete_command.split())
-    data = json.loads(stdout)
-    assert data[secret_id]["content"]["Data"]["replication-password"] == password
-
-
-async def test_empty_password(ops_test: OpsTest) -> None:
-    """Test that the password can't be set to an empty string."""
-    await set_password(ops_test, username="replication", password="")
-    password = await get_password(ops_test, username="replication")
-    # The password is 'None', BUT NOT because of SECRET_DELETED_LABEL
-    # `get_secret()` returns a None value (as the field in the secret is set to string value "None")
-    # And this true None value is turned to a string when the event is setting results.
-    assert password == "None"
-
-
 async def test_db_connection_with_empty_password(ops_test: OpsTest):
     """Test that user can't connect with empty password."""
     primary = await get_primary(ops_test, f"{APP_NAME}/0")
