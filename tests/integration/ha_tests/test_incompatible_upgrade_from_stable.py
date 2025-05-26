fe8d501fc88cf8d1a6f286369bda2975d0eece63
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2025-05-26 12:59:27 -0300

    Add new tests for incompatible upgrades
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>

---
+++ tests/integration/ha_tests/test_incompatible_upgrade_from_stable.py
@@ -0,0 +1,135 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+import logging
+
+import pytest
+from pytest_operator.plugin import OpsTest
+
+from ..helpers import (
+    APPLICATION_NAME,
+    DATABASE_APP_NAME,
+    count_switchovers,
+    get_leader_unit,
+    get_primary,
+)
+from .helpers import (
+    are_writes_increasing,
+    check_writes,
+    start_continuous_writes,
+)
+
+logger = logging.getLogger(__name__)
+
+TIMEOUT = 900
+
+
+@pytest.mark.abort_on_fail
+async def test_deploy_stable(ops_test: OpsTest) -> None:
+    """Simple test to ensure that the PostgreSQL and application charms get deployed."""
+    await ops_test.juju(
+        "deploy",
+        DATABASE_APP_NAME,
+        "-n",
+        3,
+        # TODO move to stable once we release refresh v3 to stable
+        "--channel",
+        "16/edge/test-refresh-v3-workload2",
+        "--base",
+        "ubuntu@24.04",
+    )
+    await ops_test.model.deploy(
+        APPLICATION_NAME,
+        num_units=1,
+        channel="latest/edge",
+    )
+    logger.info("Wait for applications to become active")
+    async with ops_test.fast_forward():
+        await ops_test.model.wait_for_idle(
+            apps=[DATABASE_APP_NAME, APPLICATION_NAME], status="active", timeout=(20 * 60)
+        )
+    assert len(ops_test.model.applications[DATABASE_APP_NAME].units) == 3
+
+
+@pytest.mark.abort_on_fail
+async def test_pre_refresh_check(ops_test: OpsTest) -> None:
+    """Test that the pre-refresh-check action runs successfully."""
+    logger.info("Get leader unit")
+    leader_unit = await get_leader_unit(ops_test, DATABASE_APP_NAME)
+    assert leader_unit is not None, "No leader unit found"
+
+    logger.info("Run pre-refresh-check action")
+    action = await leader_unit.run_action("pre-refresh-check")
+    await action.wait()
+
+
+@pytest.mark.abort_on_fail
+async def test_upgrade_from_stable(ops_test: OpsTest, charm):
+    """Test updating from stable channel."""
+    # Start an application that continuously writes data to the database.
+    logger.info("starting continuous writes to the database")
+    await start_continuous_writes(ops_test, DATABASE_APP_NAME)
+
+    # Check whether writes are increasing.
+    logger.info("checking whether writes are increasing")
+    await are_writes_increasing(ops_test)
+
+    primary_name = await get_primary(ops_test, f"{DATABASE_APP_NAME}/0")
+    initial_number_of_switchovers = count_switchovers(ops_test, primary_name)
+
+    application = ops_test.model.applications[DATABASE_APP_NAME]
+
+    logger.info("Refresh the charm")
+    await application.refresh(path=charm)
+
+    logger.info("Wait for upgrade to start")
+    await ops_test.model.block_until(lambda: application.status == "blocked", timeout=TIMEOUT)
+
+    logger.info("Wait for refresh to block due to compatibility checks")
+    async with ops_test.fast_forward("60s"):
+        await ops_test.model.wait_for_idle(
+            apps=[DATABASE_APP_NAME], idle_period=30, timeout=TIMEOUT
+        )
+
+    assert "Refresh incompatible" in application.status_message, (
+        "Application refresh not blocked due to incompatibility"
+    )
+
+    # Highest to lowest unit number
+    refresh_order = sorted(
+        application.units, key=lambda unit: int(unit.name.split("/")[1]), reverse=True
+    )
+    action = await refresh_order[0].run_action(
+        "force-refresh-start", **{"check-compatibility": False}
+    )
+    await action.wait()
+
+    logger.info("Wait for first unit to upgrade")
+    async with ops_test.fast_forward("60s"):
+        await ops_test.model.wait_for_idle(
+            apps=[DATABASE_APP_NAME], idle_period=30, timeout=TIMEOUT
+        )
+
+    logger.info("Run resume-refresh action")
+    action = await refresh_order[1].run_action("resume-refresh")
+    await action.wait()
+
+    logger.info("Wait for upgrade to complete")
+    async with ops_test.fast_forward("60s"):
+        await ops_test.model.wait_for_idle(
+            apps=[DATABASE_APP_NAME], status="active", idle_period=30, timeout=TIMEOUT
+        )
+
+    # Check whether writes are increasing.
+    logger.info("checking whether writes are increasing")
+    await are_writes_increasing(ops_test)
+
+    # Verify that no writes to the database were missed after stopping the writes
+    # (check that all the units have all the writes).
+    logger.info("checking whether no writes were lost")
+    await check_writes(ops_test)
+
+    logger.info("checking the number of switchovers")
+    final_number_of_switchovers = count_switchovers(ops_test, primary_name)
+    assert (final_number_of_switchovers - initial_number_of_switchovers) <= 2, (
+        "Number of switchovers is greater than 2"
+    )
