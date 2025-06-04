b2ac81bbda42a835613fc67ea903c3353a9d8c78
Dragomir Penev <dragomir.penev@canonical.com>
2025-06-04 12:33:56 +0300

    Squashed commit of the following:
    commit da3dd59e605628a856ee82d5f576252a9bbc7dcb
    Author: Dragomir Penev <dragomir.penev@canonical.com>
    Date:   Wed Jun 4 01:05:15 2025 +0300
    
        Add sleep interval
    
    commit 141efaf42fb9561403166d15cad53b18afbe16d8
    Author: Dragomir Penev <dragomir.penev@canonical.com>
    Date:   Wed Jun 4 00:35:32 2025 +0300
    
        Don't defer on raft removal
    
    commit bcecb8c8ea6d316ba13e0739efce743db9c079d8
    Author: Dragomir Penev <dragomir.penev@canonical.com>
    Date:   Tue Jun 3 23:44:30 2025 +0300
    
        Log raft removal error
    
    commit d12515f3d2fe02154e839c95820645c47d4d3beb
    Author: Dragomir Penev <dragomir.penev@canonical.com>
    Date:   Tue Jun 3 18:17:40 2025 +0300
    
        Use peer addrs directly
    
    commit 03e5031407abd4feb8fff1799c4a58120e4b5335
    Author: Dragomir Penev <dragomir.penev@canonical.com>
    Date:   Tue Jun 3 16:47:54 2025 +0300
    
        Use peer cert and key
    
    commit 762a9e3d0c12375479e7c75dd2e8bca5dcb4d247
    Author: Dragomir Penev <dragomir.penev@canonical.com>
    Date:   Tue Jun 3 15:56:19 2025 +0300
    
        Disable unit tests
    
    commit ee091df9d3efaf28be5b2633bb7f751e8f923447
    Author: Dragomir Penev <dragomir.penev@canonical.com>
    Date:   Tue Jun 3 15:50:40 2025 +0300
    
        Try to suppress update status
    
    commit 24cdb8f263068f52c9b6bcc884f4e8b43ea80f3b
    Merge: f3befdb66 fb27850a9
    Author: Dragomir Penev <dragomir.penev@canonical.com>
    Date:   Tue Jun 3 15:49:31 2025 +0300
    
        Merge branch 'tlsv4' into tlsv4-conditional-validation
    
    commit f3befdb66ff2b7c4808475c10d7e3dd984e1bd44
    Author: Dragomir Penev <dragomir.penev@canonical.com>
    Date:   Tue Jun 3 14:29:01 2025 +0300
    
        Try to wait for idle again
    
    commit a9e9d504aed7c65bf8bb35db4d8e2e73e4150bd5
    Author: Dragomir Penev <dragomir.penev@canonical.com>
    Date:   Tue Jun 3 13:42:06 2025 +0300
    
        Bump timeout
    
    commit 90a8f7d428bac133a9db21b88a163b927cfcbfef
    Author: Dragomir Penev <dragomir.penev@canonical.com>
    Date:   Mon Jun 2 23:59:12 2025 +0300
    
        Add reraising
    
    commit aa6a1ea78b030e5e348c996d7a4b6bfc6c7ac6ba
    Author: Dragomir Penev <dragomir.penev@canonical.com>
    Date:   Mon Jun 2 21:36:39 2025 +0300
    
        Try bumping timeout
    
    commit cf6e9980c6a5c246d07228a79b7da62be51b3a68
    Author: Dragomir Penev <dragomir.penev@canonical.com>
    Date:   Mon Jun 2 18:49:36 2025 +0300
    
        Try to update ips first
    
    commit 08cea05d77cd5002e23dbbfecdf8823c2e353ffa
    Author: Dragomir Penev <dragomir.penev@canonical.com>
    Date:   Mon Jun 2 17:29:06 2025 +0300
    
        Try to update conf on IP change without validation
    
    commit 684070725873edf5b517a19d93957c2bb9aadf7e
    Author: Dragomir Penev <dragomir.penev@canonical.com>
    Date:   Mon Jun 2 16:55:30 2025 +0300
    
        Try to log the exception
    
    commit e67489bb0987895f2359dc4bd96e49bc635f7826
    Author: Dragomir Penev <dragomir.penev@canonical.com>
    Date:   Mon Jun 2 16:35:00 2025 +0300
    
        Debug network cut

--- src/charm.py
+++ src/charm.py
@@ -618,16 +618,6 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
         else:
             return primary_endpoint
 
-    def get_hostname_by_unit(self, _) -> str:
-        """Create a DNS name for a PostgreSQL unit.
-
-        Returns:
-            A string representing the hostname of the PostgreSQL unit.
-        """
-        # For now, as there is no DNS hostnames on VMs, and it would also depend on
-        # the underlying provider (LXD, MAAS, etc.), the unit IP is returned.
-        return self._unit_ip
-
     def _on_get_primary(self, event: ActionEvent) -> None:
         """Get primary instance."""
         try:
@@ -1011,7 +1001,7 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
         # hook, the configuration is updated and the service is started - or only
         # reloaded in the other units).
         stored_ip = self.unit_peer_data.get("ip")
-        current_ip = self.get_hostname_by_unit(None)
+        current_ip = self._unit_ip
         if stored_ip is None:
             self.unit_peer_data.update({"ip": current_ip})
             return False
@@ -1428,6 +1418,11 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
             event.defer()
             return
 
+        if self._update_member_ip():
+            # Update the sync-standby endpoint in the async replication data.
+            self.async_replication.update_async_replication_data()
+            return
+
         try:
             self._validate_config_options()
             # update config on every run
@@ -1448,11 +1443,6 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
 
         if self.is_blocked and "Configuration Error" in self.unit.status.message:
             self.set_unit_status(ActiveStatus())
-
-        if self._update_member_ip():
-            # Update the sync-standby endpoint in the async replication data.
-            self.async_replication.update_async_replication_data()
-            return
 
         # Update the sync-standby endpoint in the async replication data.
         self.async_replication.update_async_replication_data()
@@ -1584,7 +1574,7 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
         if not self.get_secret(UNIT_SCOPE, "internal-cert"):
             self.tls.generate_internal_peer_cert()
 
-        self.unit_peer_data.update({"ip": self.get_hostname_by_unit(None)})
+        self.unit_peer_data.update({"ip": self._unit_ip})
 
         # Open port
         try:
