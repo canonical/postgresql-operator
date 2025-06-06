6b47ef306133f1907e18365d5d931caa54a5a252
Dragomir Penev <dragomir.penev@canonical.com>
2025-06-06 17:33:50 +0300

    Use current host

--- lib/charms/postgresql_k8s/v0/postgresql.py
+++ lib/charms/postgresql_k8s/v0/postgresql.py
@@ -803,7 +803,9 @@ END; $$;"""
         connection = None
         host = self.current_host if current_host else None
         try:
-            with self._connect_to_database() as connection, connection.cursor() as cursor:
+            with self._connect_to_database(
+                database_host=host
+            ) as connection, connection.cursor() as cursor:
                 cursor.execute(
                     "SELECT usename "
                     "FROM pg_catalog.pg_user "
