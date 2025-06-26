65c54161d61081096d28b17a4f32758984216735
Alex Lutay <1928266+taurus-forever@users.noreply.github.com>
2025-06-27 01:59:35 +0200

    Revert LIBPATCH back to 0

--- lib/charms/postgresql_k8s/v1/postgresql.py
+++ lib/charms/postgresql_k8s/v1/postgresql.py
@@ -34,7 +34,7 @@ LIBAPI = 1
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 1
+LIBPATCH = 0
 
 # Groups to distinguish HBA access
 ACCESS_GROUP_IDENTITY = "identity_access"
