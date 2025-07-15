0d40c54fad8ac4974bc659ef49dd6ab872f76e19
Gere_X <gergely.gere@gmail.com>
2025-07-15 09:02:03 +0200

    add tests

--- tests/unit/test_async_replication.py
+++ tests/unit/test_async_replication.py
@@ -805,3 +805,22 @@ def test_handle_forceful_promotion():
         "test-app isn't reachable. Pass `force=true` to promote anyway."
     )
     assert result is False
+    # 3.
+    mock_charm = MagicMock()
+    mock_event = MagicMock()
+
+    mock_event.params.get.return_value = False
+
+    relation = PostgreSQLAsyncReplication(mock_charm)
+
+    relation._relation = MagicMock()
+    relation._relation.app = MagicMock()
+    relation._relation.app.name = "test-app"
+
+    relation.get_all_primary_cluster_endpoints = MagicMock(return_value=[1, 2, 3])
+
+    mock_charm._patroni.get_primary.side_effect = None
+
+    result = relation._handle_forceful_promotion(mock_event)
+
+    assert result is True
