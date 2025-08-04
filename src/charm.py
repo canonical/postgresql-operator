1609fa2f4126da7bf2ce0af4e6c2937d575ff1d9
Dragomir Penev <6687393+dragomirp@users.noreply.github.com>
2025-08-04 16:40:16 +0300

    [DPE-7594] Add custom users to pg_hba filter (#1072)
    * Filter out only the trigger users
    
    * Update lib
    
    * Add unit test
    
    * Add back database check

--- src/charm.py
+++ src/charm.py
@@ -2497,15 +2497,21 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
             })
             return user_database_map
         try:
-            for user in self.postgresql.list_users_from_relation(
-                current_host=self.is_connectivity_enabled
-            ):
-                databases = ",".join(
+            for user in self.postgresql.list_users(current_host=self.is_connectivity_enabled):
+                if user in (
+                    "backup",
+                    "monitoring",
+                    "operator",
+                    "postgres",
+                    "replication",
+                    "rewind",
+                ):
+                    continue
+                if databases := ",".join(
                     self.postgresql.list_accessible_databases_for_user(
                         user, current_host=self.is_connectivity_enabled
                     )
-                )
-                if databases:
+                ):
                     user_database_map[user] = databases
                 else:
                     logger.debug(f"User {user} has no databases to connect to")
