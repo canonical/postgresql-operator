17822f20bbcdfff6b0393f7d27c7d03441dc00cd
Alex Lutay <1928266+taurus-forever@users.noreply.github.com>
2025-08-19 12:42:55 +0200

    DPE-7968 Bump snap revision (remove python3-boto3 for CVE-2023-37920)

--- src/constants.py
+++ src/constants.py
@@ -35,7 +35,7 @@ POSTGRESQL_SNAP_NAME = "charmed-postgresql"
 SNAP_PACKAGES = [
     (
         POSTGRESQL_SNAP_NAME,
-        {"revision": {"aarch64": "186", "x86_64": "184"}},
+        {"revision": {"aarch64": "221", "x86_64": "220"}},
     )
 ]
 
