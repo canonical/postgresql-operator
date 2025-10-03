b130f70534e3cbbe693f96b166a4aad854fd0bd6
Dragomir Penev <dragomir.penev@canonical.com>
2025-10-03 17:13:09 +0300

    Try to cleanup only if the test didn't fail

--- tests/integration/ha_tests/conftest.py
+++ tests/integration/ha_tests/conftest.py
@@ -21,20 +21,23 @@ from .helpers import (
 )
 
 
-@pytest.fixture()
-async def continuous_writes(ops_test: OpsTest) -> None:
+@pytest.fixture
+async def continuous_writes(ops_test: OpsTest, request) -> None:
     """Deploy the charm that makes continuous writes to PostgreSQL."""
     yield
-    # Clear the written data at the end.
-    for attempt in Retrying(stop=stop_after_delay(60 * 5), wait=wait_fixed(3), reraise=True):
-        with attempt:
-            action = (
-                await ops_test.model.applications[APPLICATION_NAME]
-                .units[0]
-                .run_action("clear-continuous-writes")
-            )
-            await action.wait()
-            assert action.results["result"] == "True", "Unable to clear up continuous_writes table"
+    if not request.session.testsfailed:
+        # Clear the written data at the end.
+        for attempt in Retrying(stop=stop_after_delay(60 * 5), wait=wait_fixed(3), reraise=True):
+            with attempt:
+                action = (
+                    await ops_test.model.applications[APPLICATION_NAME]
+                    .units[0]
+                    .run_action("clear-continuous-writes")
+                )
+                await action.wait()
+                assert action.results["result"] == "True", (
+                    "Unable to clear up continuous_writes table"
+                )
 
 
 @pytest.fixture()
