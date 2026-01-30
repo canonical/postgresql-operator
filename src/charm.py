d439e3d26620e37f4a077ccef54adbb8747c2d50
Dragomir Penev <dragomir.penev@canonical.com>
2026-01-30 20:35:37 +0200

    Port from 16

--- src/charm.py
+++ src/charm.py
@@ -125,6 +125,7 @@ logger = logging.getLogger(__name__)
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logging.getLogger("httpcore").setLevel(logging.WARNING)
 logging.getLogger("asyncio").setLevel(logging.WARNING)
+logging.getLogger("boto").setLevel(logging.WARNING)
 
 PRIMARY_NOT_REACHABLE_MESSAGE = "waiting for primary to be reachable from this unit"
 EXTENSIONS_DEPENDENCY_MESSAGE = "Unsatisfied plugin dependencies. Please check the logs"
--- tests/integration/test_backups_pitr_ceph.py
+++ tests/integration/test_backups_pitr_ceph.py
@@ -6,12 +6,11 @@ import logging
 import pytest
 from pytest_operator.plugin import OpsTest
 
-from .backup_helpers import backup_operations
+from .backup_helpers import pitr_backup_operations
 from .conftest import ConnectionInformation
 from .juju_ import juju_major_version
 
-logger = logging.getLogger(__name__)
-
+S3_INTEGRATOR_APP_NAME = "s3-integrator"
 S3_INTEGRATOR_APP_NAME = "s3-integrator"
 if juju_major_version < 3:
     tls_certificates_app_name = "tls-certificates-operator"
@@ -22,7 +21,7 @@ else:
     tls_channel = "1/stable"
     tls_config = {"ca-common-name": "Test CA"}
 
-backup_id, value_before_backup, value_after_backup = "", None, None
+logger = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope="session")
@@ -46,9 +45,12 @@ def cloud_configs(microceph: ConnectionInformation):
     }
 
 
-async def test_backup_ceph(ops_test: OpsTest, cloud_configs, cloud_credentials, charm) -> None:
-    """Build and deploy two units of PostgreSQL in microceph, test backup and restore actions."""
-    await backup_operations(
+@pytest.mark.abort_on_fail
+async def test_pitr_backup_ceph(
+    ops_test: OpsTest, cloud_configs, cloud_credentials, charm
+) -> None:
+    """Build, deploy two units of PostgreSQL and do backup in AWS. Then, write new data into DB, switch WAL file and test point-in-time-recovery restore action."""
+    await pitr_backup_operations(
         ops_test,
         S3_INTEGRATOR_APP_NAME,
         tls_certificates_app_name,
