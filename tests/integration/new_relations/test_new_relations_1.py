5fa863d57dc7ddfcf204e6903a0f0df93813e28c
Dragomir Penev <dragomir.penev@canonical.com>
2026-03-04 17:57:04 +0200

    Switch to series

--- tests/integration/new_relations/test_new_relations_1.py
+++ tests/integration/new_relations/test_new_relations_1.py
@@ -265,7 +265,7 @@ async def test_two_applications_doesnt_share_the_same_relation_data(ops_test: Op
         APPLICATION_APP_NAME,
         application_name=another_application_app_name,
         channel="latest/edge",
-        base=CHARM_BASE,
+        series="jammy",
     )
 
     # Relate the new application with the database
