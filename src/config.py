3f0a9a6c87b253e69e444f528cdab2d03560b0d3
Dragomir Penev <dragomir.penev@canonical.com>
2026-03-11 19:18:56 +0200

    Expose PG stats configuration

--- src/config.py
+++ src/config.py
@@ -96,6 +96,12 @@ class CharmConfig(BaseConfigModel):
     optimizer_min_parallel_table_scan_size: int | None
     optimizer_parallel_setup_cost: float | None
     optimizer_parallel_tuple_cost: float | None
+    optimizer_pg_stat_statements_track: Literal["none", "top", "all"]
+    optimizer_pg_stat_statements_track_utility: bool
+    optimizer_pg_stat_statements_save: bool
+    optimizer_track_io_timing: bool
+    optimizer_track_wal_io_timing: bool
+    optimizer_track_functions: Literal["none", "pl", "all"]
     profile: str
     profile_limit_memory: int | None
     plugin_address_standardizer_data_us_enable: bool
