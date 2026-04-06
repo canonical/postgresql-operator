8fc3b0d4a56fa03b22927a3e2e1590775d5e68d8
andreia <andreia.velasco@canonical.com>
2026-04-06 11:16:36 +0200

    update license and copyright information

--- docs/conf.py
+++ docs/conf.py
@@ -32,9 +32,9 @@ author = "Canonical Ltd."
 # TODO: If the date must be a range, like in a software license, replace 
 # 2026 with the starting year of development and use:
 #
-# copyright = f"2026-{datetime.date.today().year}"
+copyright = f"2025-{datetime.date.today().year}"
 
-copyright = f"{datetime.date.today().year}"
+# copyright = f"{datetime.date.today().year}"
 
 # Sidebar documentation title; best kept reasonably short
 #
@@ -149,8 +149,8 @@ html_context = {
     #       it inherits the code license instead; specify it instead of 'CC-BY-SA'.
 
     "license": {
-        "name": "CC-BY-SA-3.0",
-        "url": "https://github.com/canonical/sphinx-docs-starter-pack/blob/main/LICENSE",
+        "name": "Apache License 2.0",
+        "url": "https://github.com/canonical/postgresql-operator/blob/16/edge/LICENSE",
     },
 }
 
