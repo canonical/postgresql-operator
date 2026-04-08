346bc74b3851c1b7d142cd310b59137da79a9b9d
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2026-04-08 17:21:35 -0300

    fix(async-replication): correct re-emission of relation-changed event
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>

--- src/relations/async_replication.py
+++ src/relations/async_replication.py
@@ -434,18 +434,22 @@ class PostgreSQLAsyncReplication(Object):
             return False
 
         relation = self._relation
+        if relation is None:
+            event.fail("Replication relation not found")
+            return False
 
         # Ensure the relation has at least one remote unit before trying to process unit data.
-        if len(relation.units) == 0:
+        remote_units = [unit for unit in relation.units if unit.app == relation.app]
+        if len(remote_units) == 0:
             event.fail(
                 "All units from the other cluster must publish their pod addresses in the relation data."
             )
             return False
 
-        # Check if all units from the other cluster  published their pod IPs in the relation data.
+        # Check if all units from the other cluster published their IPs in the relation data.
         # If not, fail the action telling that all units must publish their pod addresses in the
         # relation data.
-        for unit in relation.units:
+        for unit in remote_units:
             if "unit-address" not in relation.data[unit]:
                 event.fail(
                     "All units from the other cluster must publish their pod addresses in the relation data."
@@ -648,18 +652,20 @@ class PostgreSQLAsyncReplication(Object):
 
     def _re_emit_async_relation_changed_event(self) -> None:
         """Re-emit the async relation changed event."""
-        relation = self._relation
-        relation_unit = next(iter(relation.units), None)
-        if relation_unit is None:
-            logger.debug(
-                "Skipping re-emitting relation-changed event: no related units found yet."
+        if relation := self._relation:
+            relation_unit = next(
+                (unit for unit in relation.units if unit.app == relation.app), None
             )
-            return
-        getattr(self.charm.on, f"{relation.name.replace('-', '_')}_relation_changed").emit(
-            relation,
-            app=relation.app,
-            unit=relation_unit,
-        )
+            if relation_unit is None:
+                logger.debug(
+                    "Skipping re-emitting relation-changed event: no related units found yet."
+                )
+                return
+            getattr(self.charm.on, f"{relation.name.replace('-', '_')}_relation_changed").emit(
+                relation,
+                app=relation.app,
+                unit=relation_unit,
+            )
 
     def _reinitialise_pgdata(self) -> None:
         """Reinitialise the pgdata folder."""
