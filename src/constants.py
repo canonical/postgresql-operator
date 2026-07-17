d713dea5efd5d442705c0cb9f0f399f37458f544
Dragomir Penev <6687393+dragomirp@users.noreply.github.com>
2026-07-17 15:07:24 +0300

    [MISC] Update snaps (14/edge) (#1863)
    * Update snaps
    
    * Update snaps

--- src/constants.py
+++ src/constants.py
@@ -40,7 +40,7 @@ POSTGRESQL_SNAP_NAME = "charmed-postgresql"
 SNAP_PACKAGES = [
     (
         POSTGRESQL_SNAP_NAME,
-        {"revision": {"aarch64": "369", "x86_64": "370"}},
+        {"revision": {"aarch64": "392", "x86_64": "393"}},
     )
 ]
 
