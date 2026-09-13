f44ac1b1df485c3b653093bcef2ae223a93f86c4
Alex Lutay <1928266+taurus-forever@users.noreply.github.com>
2026-09-13 22:29:10 +0200

    [DPE-10968] Bump pySyncObj from 0.3.15 to to 0.3.17 for PG14 (#1955)

--- src/constants.py
+++ src/constants.py
@@ -40,7 +40,7 @@ POSTGRESQL_SNAP_NAME = "charmed-postgresql"
 SNAP_PACKAGES = [
     (
         POSTGRESQL_SNAP_NAME,
-        {"revision": {"aarch64": "401", "x86_64": "402"}},
+        {"revision": {"aarch64": "418", "x86_64": "417"}},
     )
 ]
 
