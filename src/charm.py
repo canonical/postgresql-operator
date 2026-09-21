8c9cdff6b241db1d633f38e8692abb543ed1e353
Xavier Pedrós <xavier.pedros@canonical.com>
2026-09-21 09:45:31 +0200

    Lint fix

--- src/charm.py
+++ src/charm.py
@@ -906,7 +906,8 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
         try:
             # Compare set of Patroni cluster members and Juju hosts
             # to avoid the unnecessary reconfiguration.
-            if self._patroni.cluster_members == self._hosts and self._units_ips <= self.members_ips:
+            if (self._patroni.cluster_members == self._hosts 
+                and self._units_ips <= self.members_ips):
                 logger.debug("Early exit add_members: Patroni members equal Juju hosts")
                 return
 
